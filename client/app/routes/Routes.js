e699180533de3e80790823e1ab68b251d68f8d92
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2016-07-12 11:12:30 +0000

    Route middleware for nprogress + System.import
    By wrapping the route we allow smarter use of nprogress ( e.g. not
    showing it on first load since the view is rendered on the server
    already ).
    We also allow webpack to tree shake more efficiently
    Finally, we don't require an additional file per route
    index.js is now the route itself

--- client/app/routes/Routes.js
+++ client/app/routes/Routes.js
@@ -1,15 +1,50 @@
 import App from '../components/App'
+import nprogress from 'nprogress'
+
+const routes = {};
+let firstRoute = true;
+
+// Route middleware for nprogress
+// We also avoid an additional module and allow webpack to tree shake via System.import
+function route(name, getComponentWrap) {
+  if ( typeof routes[name] === 'undefined' ) {
+    routes[name] = false;
+  }
+
+  return {
+    path: name === '/' ? undefined : name,
+    getComponent(nextState, cb) {
+      getComponentWrap(nextState, (err, module) => {
+        if ( routes[name] === false ) {
+          routes[name] = true;
+
+          if ( firstRoute ) {
+            firstRoute = false;
+          } else {
+            nprogress.done();
+          }
+        }
+        cb(null, module.default);
+      });
+    },
+    onEnter() {
+      if ( !firstRoute && !routes[name] && process.browser ) {
+        nprogress.start();
+      }
+    }
+  }
+}
 
 export default {
   childRoutes: [{
     path: '/',
     component: App,
-    indexRoute: require('./home').default,
+    indexRoute: route('/', (s, cb) => System.import('./home').then(m => cb(null, m))),
     childRoutes: [
-      require('./download').default,
-      require('./guide').default,
-      require('./source').default,
-      require('./license').default,
+      route('download', (s, cb) => System.import('./download').then(m => cb(null, m))),
+      route('guide', (s, cb) => System.import('./guide').then(m => cb(null, m))),
+      route('source', (s, cb) => System.import('./source').then(m => cb(null, m))),
+      route('license', (s, cb) => System.import('./license').then(m => cb(null, m))),
     ]
   }]
 };
