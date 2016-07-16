cea9e70e19d91ed619ebf7da2337a6e9cdb1de95
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2016-07-16 14:58:10 +0000

    Auto-update copyright year

--- client/app/components/Copyright.js
+++ client/app/components/Copyright.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default class Copyright extends React.Component {
+
   render() {
     return (
       <section className="copyright">
         <div className="container">
-          <p>Copyright &copy; 2012-2016 Lightweight Java Game Library 3</p>
+          <p>Copyright &copy; 2012-{new Date().getFullYear()} Lightweight Java Game Library 3</p>
           <p>Licensed under GPL</p>
         </div>
       </section>
