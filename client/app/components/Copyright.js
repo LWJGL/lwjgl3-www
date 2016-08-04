9ec95247f5996fbe99b7fc7066d735a0aa6d3e07
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2016-08-04 21:40:02 +0000

    LWJGL is licensed under BSD

--- client/app/components/Copyright.js
+++ client/app/components/Copyright.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default class Copyright extends React.PureComponent {
 
@@ -7,7 +8,7 @@ export default class Copyright extends React.PureComponent {
       <section className="copyright">
         <div className="container">
           <p>Copyright &copy; 2012-{new Date().getFullYear()} Lightweight Java Game Library 3</p>
-          <p>Licensed under GPL</p>
+          <p>Licensed under <Link to="/license">BSD</Link></p>
         </div>
       </section>
     );
--- client/app/routes/license/index.js
+++ client/app/routes/license/index.js
@@ -18,36 +18,19 @@ export default class LicenseRoute extends React.Component {
 
           <article className="row">
             <div className="col-lg-6 col-xs-12">
-              <p>Redistribution and use in source and binary forms, with or without
-                modification, are permitted provided that the following conditions are
-                met:</p>
+              <p>Copyright &copy; 2012-{new Date().getFullYear()} Lightweight Java Game Library
+              <br/>All rights reserved.</p>
 
-              <ul>
-                <li>Redistributions of source code must retain the above copyright
-                  notice, this list of conditions and the following disclaimer.
-                </li>
-                <li>Redistributions in binary form must reproduce the above copyright
-                  notice, this list of conditions and the following disclaimer in the
-                  documentation and/or other materials provided with the distribution.
-                </li>
-                <li>Neither the name of 'Light Weight Java Game Library' nor the names of
-                  its contributors may be used to endorse or promote products derived
-                  from this software without specific prior written permission.
-                </li>
-              </ul>
+              <p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p>
+
+              <ol>
+                <li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</li>
+                <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</li>
+                <li>Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.</li>
+              </ol>
             </div>
             <div className="col-lg-6 col-xs-12">
-              <p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-                "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-                TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-                PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-                EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-                PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-                PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-                LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-                NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-                SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>
+              <p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>
             </div>
           </article>
         </section>
