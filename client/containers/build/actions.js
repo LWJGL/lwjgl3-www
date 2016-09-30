e53506f87d1ee0f5e32c587b7de9111a95e5cab8
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2016-09-30 16:01:54 +0000

    ControlledCheckbox Redux + ControlledAlert

--- client/containers/build/actions.js
+++ client/containers/build/actions.js
@@ -1,11 +1,13 @@
-import * as actionTypes from './actionTypes'
+import * as $ from './actionTypes'
 
-export const changeType = type => ({
-  type: actionTypes.SELECT_TYPE,
-  build: type,
-});
+export const changeType = type => ({type: $.SELECT_TYPE, build: type});
+export const changeMode = mode => ({type: $.SELECT_MODE, mode});
 
-export const changeMode = mode => ({
-  type: actionTypes.SELECT_MODE,
-  mode,
-});
+export const toggleDescriptions = enabled => ({type: $.TOGGLE_DESCRIPTIONS, descriptions: enabled});
+export const toggleSource = enabled => ({type: $.TOGGLE_SOURCE, source: enabled});
+export const toggleJavadoc = enabled => ({type: $.TOGGLE_JAVADOC, javadoc: enabled});
+export const toggleCompact = enabled => ({type: $.TOGGLE_COMPACT, compact: enabled});
+export const toggleHardcoded = enabled => ({type: $.TOGGLE_HARDCODED, hardcoded: enabled});
+
+export const errorSet = (message, severity="danger") => ({type: $.ERROR_SET, error: { message, severity }});
+export const errorReset = () => ({type: $.ERROR_SET, error: null});
--- client/containers/build/index.js
+++ client/containers/build/index.js
@@ -8,19 +8,63 @@ import Panel from '../../components/Panel'
 // import BuildScript from './BuildScript'
 // import ControlledPanel from '../../components/ControlledPanel'
 // import ControlledRadio from '../../components/ControlledRadio'
-// import ControlledCheckbox from '../../components/ControlledCheckbox'
+import ControlledAlert from '../../components/ControlledAlert'
+import ControlledCheckbox from '../../components/ControlledCheckbox'
 
 const isBuildSelected = state => state.build.build !== null;
 const isModeGradle = state => state.build.mode === 'gradle';
 const isModeNotZip = state => state.build.mode !== 'zip';
 const isBuildRelease = state => state.build.buildType === 'release';
-const getContentsClass = state => classnames("col-xs-12", {
+const getContentsClass = state => classnames(
+    "col-xs-12",
+  {
     "col-lg-3": state.build.mode !== 'zip',
     "col-lg-5": state.build.mode === 'zip'
-  });
+  }
+);
+
+import * as $$ from './actions'
+
+const fields = {
+  descriptions: {
+    label: "Show descriptions",
+    checked: state => state.build.descriptions,
+    action: $$.toggleDescriptions,
+  },
+  source: {
+    label: "Include source",
+    checked: state => state.build.source,
+    action: $$.toggleSource,
+    hidden: isModeNotZip
+  },
+  javadoc: {
+    label: "Include JavaDoc",
+    checked: state => state.build.javadoc,
+    action: $$.toggleJavadoc,
+    hidden: isModeNotZip
+  },
+  compact: {
+    label: "Compact Mode",
+    checked: state => state.build.compact,
+    action: $$.toggleCompact,
+    hidden: state => state.build.mode !== 'maven'
+  },
+  hardcoded: {
+    label: "Do not use variables",
+    checked: state => state.build.hardcoded,
+    action: $$.toggleHardcoded,
+    hidden: state => state.build.mode === 'zip'
+  },
+};
 
 const BuildContainer = props => (
   <div>
+
+    <ControlledAlert
+      selector={state => state.build.error}
+      reset={$$.errorReset}
+    />
+
     <div className="row">
       <div className="col-lg-4 col-xs-12">
         <BuildType build="release"/>
@@ -44,11 +88,11 @@ const BuildContainer = props => (
               {/*<ControlledRadio name="preset"/>*/}
 
               <h2 className="m-b-1">Options</h2>
-              {/*<ControlledCheckbox name="descriptions"/>*/}
-              {/*<ControlledCheckbox name="source"/>*/}
-              {/*<ControlledCheckbox name="javadoc"/>*/}
-              {/*<ControlledCheckbox name="compact"/>*/}
-              {/*<ControlledCheckbox name="hardcoded"/>*/}
+              <ControlledCheckbox spec={fields.descriptions}/>
+              <ControlledCheckbox spec={fields.source}/>
+              <ControlledCheckbox spec={fields.javadoc}/>
+              <ControlledCheckbox spec={fields.compact}/>
+              <ControlledCheckbox spec={fields.hardcoded}/>
 
               <Panel predicate={isModeGradle}>
                 <h2 className="m-b-1">Language</h2>
