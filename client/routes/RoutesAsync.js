cac8bdb98b6453aef88efd382d6e111ac71ffafc
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2016-10-25 11:44:59 +0000

    Lifecycle changes to asyncRoute, avoid cWM

--- client/routes/RoutesAsync.js
+++ client/routes/RoutesAsync.js
@@ -6,71 +6,66 @@ import preserver from './Preserver'
 
 let firstRoute = true;
 
-function asyncRoute(getComponent) {
-  return class AsyncComponent extends React.Component {
-    static Component = null;
-    mounted = false;
+const asyncRoute = getComponent => class AsyncComponent extends React.Component {
+  static Component = null;
 
-    state = {
-      Component: AsyncComponent.Component
-    };
+  mounted = false;
+  state = {
+    Component: AsyncComponent.Component
+  };
 
-    componentWillMount() {
-      document.documentElement.scrollTop = 0;
-      if ( this.state.Component === null ) {
-        if ( !firstRoute ) {
-          nprogress.start();
-        }
-        getComponent().then(m => m.default).then(Component => {
-          if ( firstRoute ) {
-            firstRoute = false;
-            if ( process.env.NODE_ENV === 'production' ) {
-              loadJS('https://www.google-analytics.com/analytics.js');
-            }
-          } else {
-            //noinspection JSUnresolvedFunction
-            nprogress.done();
+  componentDidMount() {
+    this.mounted = true;
+    document.documentElement.scrollTop = 0;
+    if ( this.state.Component === null ) {
+      if ( !firstRoute ) {
+        nprogress.start();
+      }
+      getComponent().then(m => m.default).then(Component => {
+        if ( firstRoute ) {
+          firstRoute = false;
+          if ( process.env.NODE_ENV === 'production' ) {
+            loadJS('https://www.google-analytics.com/analytics.js');
           }
-          AsyncComponent.Component = Component;
-          if ( this.mounted ) {
-            this.setState({Component});
-          }
-        })
-      }
-    }
-
-    componentDidMount() {
-      this.mounted = true;
-    }
-
-    componentWillUnmount() {
-      this.mounted = false;
-    }
-
-    render() {
-      const {Component} = this.state;
-      const {location} = this.props;
-
-      if ( Component !== null ) {
-        if ( process.env.NODE_ENV === 'production' ) {
-          ga('send', 'pageview', `${location.pathname}${location.search}`);
+        } else {
+          //noinspection JSUnresolvedFunction
+          nprogress.done();
         }
-
-        return <Component {...this.props} />
-      }
-
-      if ( preserver.has() ) {
-        return preserver.render();
-      } else {
-        return (
-          <div className="container text-xs-center" style={{padding:'25vh 0'}}>
-            <LoaderSpinner size={128} delay={true} />
-          </div>
-        )
-      }
+        AsyncComponent.Component = Component;
+        if ( this.mounted ) {
+          this.setState({Component});
+        }
+      })
     }
   }
-}
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  render() {
+    const {Component} = this.state;
+
+    if ( Component !== null ) {
+      if ( process.env.NODE_ENV === 'production' ) {
+        const {location} = this.props;
+        ga('send', 'pageview', `${location.pathname}${location.search}`);
+      }
+
+      return <Component {...this.props} />
+    }
+
+    if ( preserver.has() ) {
+      return preserver.render();
+    } else {
+      return (
+        <div className="container text-xs-center" style={{padding:'25vh 0'}}>
+          <LoaderSpinner size={128} delay={true} />
+        </div>
+      )
+    }
+  }
+};
 
 export const Home = asyncRoute(() => System.import('./home/index'));
 export const Download = asyncRoute(() => System.import('./download/index'));
