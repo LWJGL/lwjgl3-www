08638eb1b76f0816c94b1d4d9f0c1d7343efbb9f
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2016-10-26 20:37:45 +0000

    Disable Public Key Pinning for now and enable HSTS

--- server/server.js
+++ server/server.js
@@ -108,24 +108,27 @@ app.use(helmet({
     action: 'sameorigin'
   },
   hidePoweredBy: false,
+  /*
   hpkp: {
     maxAge: 7776000,
     sha256s: [
-      '2b071c59a0a0ae76b0eadb2bad23bad4580b69c3601b630c2eaf0613afa83f92',
-      'f7ecded5c66047d28ed6466b543c40e0743abe81d109254dcf845d4c2c7853c5',
-      '36abc32656acfc645c61b71613c4bf21c787f5cabbee48348d58597803d7abc9',
-      '7f4296fc5b6a4e3b35d3c369623e364ab1af381d8fa7121533c9d6c633ea2461',
-      'fbe3018031f9586bcbf41727e417b7d1c45c2f47f93be372a17b96b50757d5a2',
+      // https://www.amazontrust.com/repository/
+      new Buffer('2b071c59a0a0ae76b0eadb2bad23bad4580b69c3601b630c2eaf0613afa83f92').toString('base64'),
+      new Buffer('f7ecded5c66047d28ed6466b543c40e0743abe81d109254dcf845d4c2c7853c5').toString('base64'),
+      new Buffer('36abc32656acfc645c61b71613c4bf21c787f5cabbee48348d58597803d7abc9').toString('base64'),
+      new Buffer('7f4296fc5b6a4e3b35d3c369623e364ab1af381d8fa7121533c9d6c633ea2461').toString('base64'),
+      new Buffer('fbe3018031f9586bcbf41727e417b7d1c45c2f47f93be372a17b96b50757d5a2').toString('base64'),
     ],
-    reportUri: 'https://www.amazontrust.com/#abuseContact',
-    reportOnly: true,
-    setIf: (req, res) => app.locals.production && req.hostname === 'www.lwjgl.org' && req.query.hpkp
+    includeSubDomains: false,
+    setIf: (req, res) => app.locals.production && req.hostname === 'www.lwjgl.org'
   },
+  */
   hsts: {
     maxAge: 31536000,
     includeSubDomains: false,
-    preload: true,  // includeSubDomains must be true for preloading to be approved
-    setIf: (req, res) => app.locals.production && req.hostname === 'www.lwjgl.org' && req.query.hsts
+    // TODO: includeSubDomains must be true for preloading to be approved
+    preload: true,
+    setIf: (req, res) => app.locals.production && req.hostname === 'www.lwjgl.org'
   },
   ieNoOpen: false,
   noSniff: true,
