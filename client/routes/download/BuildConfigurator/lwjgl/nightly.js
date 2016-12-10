12f4a9ab49c6464454b057c521f06d91fe711b7d
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2016-12-10 14:40:58 +0000

    Promote nightly to stable, upgrade node-sass to v4

--- client/routes/download/BuildConfigurator/lwjgl/nightly.js
+++ client/routes/download/BuildConfigurator/lwjgl/nightly.js
@@ -6,17 +6,6 @@ export default (stable) => {
 
   const artifacts = {...stable};
 
-  artifacts.byId = {
-    ...artifacts.byId,
-    'lwjgl-assimp': {
-        id: 'lwjgl-assimp',
-        title: 'Assimp bindings',
-        description: 'A portable Open Source library to import various well-known 3D model formats in a uniform manner.',
-        natives: NATIVE_ALL,
-        website: 'http://www.assimp.org/',
-    },
-  };
-
   artifacts.byId['lwjgl-opengl'] = {
     ...artifacts.byId['lwjgl-opengl'],
     natives: NATIVE_ALL,
@@ -26,8 +15,6 @@ export default (stable) => {
     ...artifacts.byId['lwjgl-opengles'],
     natives: NATIVE_ALL,
   };
-
-  artifacts.allIds = Object.keys(artifacts.byId).sort();
 
   return artifacts;
 };
--- client/routes/download/BuildConfigurator/lwjgl/stable.js
+++ client/routes/download/BuildConfigurator/lwjgl/stable.js
@@ -1,3 +1,23 @@
-export default (prev) => {
-  return prev;
+import {
+  NATIVE_ALL
+} from '../constants'
+
+export default (release) => {
+
+  const artifacts = {...release};
+
+  artifacts.byId = {
+    ...artifacts.byId,
+    'lwjgl-assimp': {
+        id: 'lwjgl-assimp',
+        title: 'Assimp bindings',
+        description: 'A portable Open Source library to import various well-known 3D model formats in a uniform manner.',
+        natives: NATIVE_ALL,
+        website: 'http://www.assimp.org/',
+    },
+  };
+
+  artifacts.allIds = Object.keys(artifacts.byId).sort();
+
+  return artifacts;
 };
