47d0a8f5ca8916b09bb5e26e1e54af6ba3dcbc78
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-12-29 14:33:17 +0200

    Start LWJGL 3.1.2

--- client/routes/download/BuildConfigurator/lwjgl/nightly.js
+++ client/routes/download/BuildConfigurator/lwjgl/nightly.js
@@ -4,6 +4,16 @@ import {
 
 export default (prev) => ({
   ...prev,
-  version: '3.1.1',
+  version: '3.1.2',
   alias: 'nightly',
+  byId: {
+    ...prev.byId,
+    'lwjgl-yoga': {
+      id: 'lwjgl-yoga',
+      title: 'Yoga bindings',
+      description: 'An open-source, cross-platform layout library that implements Flexbox.',
+      natives: NATIVE_ALL,
+      website: 'https://facebook.github.io/yoga/',
+    }
+  }
 })
