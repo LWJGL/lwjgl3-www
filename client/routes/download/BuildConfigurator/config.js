08ef3db5f6392c3845893ac593cdd16fd955a945
httpdigest <kburjack@googlemail.com>
2017-01-01 17:02:52 +0100

    Update JOML version to 1.9.1

--- client/routes/download/BuildConfigurator/config.js
+++ client/routes/download/BuildConfigurator/config.js
@@ -174,7 +174,7 @@ const config = {
         maven: {
           groupId: 'org.joml',
           artifactId: 'joml',
-          version: '1.9.0',
+          version: '1.9.1',
         },
       },
       'steamworks4j': {
