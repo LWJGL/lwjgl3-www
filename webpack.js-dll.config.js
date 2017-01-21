f0a1156b3afdde7e39700989794a64c7dfdd9b8b
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-01-21 19:21:18 +0000

    Remove babel-loader from the vendor build

--- webpack.js-dll.config.js
+++ webpack.js-dll.config.js
@@ -33,18 +33,6 @@ const config = {
     filename: '[name].js',
     library: 'vendor',
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: [
-          path.resolve(__dirname, 'node_modules')
-        ],
-        include: __dirname,
-        loader: 'babel-loader?cacheDirectory'
-      }
-    ]
-  },
   plugins: [
     new DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
