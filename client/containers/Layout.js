221872dd3f4c6305d339eeffb935aaa8f4f36ea5
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-03-26 11:05:36 +0000

    Test formatting with Prettier

--- client/containers/Layout.js
+++ client/containers/Layout.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import Switch from 'react-router-dom/Switch'
-import Route from 'react-router-dom/Route'
-import Helmet from 'react-helmet'
-import Header from './Header'
-import Footer from './Footer'
-import Copyright from './Copyright'
+import React from 'react';
+import Switch from 'react-router-dom/Switch';
+import Route from 'react-router-dom/Route';
+import Helmet from 'react-helmet';
+import Header from './Header';
+import Footer from './Footer';
+import Copyright from './Copyright';
 
 // Routes
-import { Home, Download, Guide, Source, License } from '../routes'
-import Miss404 from '../routes/miss404'
+import { Home, Download, Guide, Source, License } from '../routes';
+import Miss404 from '../routes/miss404';
 
-const Layout = () => (
-  [
-    <Helmet titleTemplate="%s - LWJGL" defaultTitle="LWJGL - Lightweight Java Game Library" />,
-    <Header />,
+const Layout = () => [
+  <Helmet titleTemplate="%s - LWJGL" defaultTitle="LWJGL - Lightweight Java Game Library" />,
+  <Header />,
 
-    <Switch>
-      <Route path="/" component={Home} exact={true} />
-      <Route path="/download" component={Download} exact={true} />
-      <Route path="/guide" component={Guide} exact={true} />
-      <Route path="/source" component={Source} exact={true} />
-      <Route path="/license" component={License} exact={true} />
-      <Route component={Miss404} />
-    </Switch>,
+  <Switch>
+    <Route path="/" component={Home} exact={true} />
+    <Route path="/download" component={Download} exact={true} />
+    <Route path="/guide" component={Guide} exact={true} />
+    <Route path="/source" component={Source} exact={true} />
+    <Route path="/license" component={License} exact={true} />
+    <Route component={Miss404} />
+  </Switch>,
 
-    <Footer />,
-    <Copyright />
-  ]
-);
+  <Footer />,
+  <Copyright />,
+];
 
-export default Layout
+export default Layout;
