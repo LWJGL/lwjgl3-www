df81b63f76e767d4641725ea98dddbaff47bcc5e
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-04-03 16:20:11 +0000

    Upgrade deps

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Server
 const fs = require('fs');
@@ -12,8 +12,8 @@ const proxy = require('http-proxy-middleware');
 
 // AWS
 const AWS = require('aws-sdk');
-AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.update({region: 'us-east-1'});
+AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
+AWS.config.update({ region: 'us-east-1' });
 
 // Lib
 const cloudFrontSubnets = require('./cloudfront-subnets.json');
@@ -33,7 +33,7 @@ app.locals.production = !app.locals.development;
 
 const manifest = app.locals.production ? require('../public/js/manifest.json') : {};
 
-if ( app.locals.production ) {
+if (app.locals.production) {
   // Convert chunks object to JSON so we can inject it in the page as <script>
   manifest.chunksSerialized = JSON.stringify(manifest.chunks);
 } else {
@@ -60,42 +60,45 @@ app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal', ...cloudFrontSub
 app.use(helmet(helmetConfig(app.locals.production)));
 app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 
-if ( app.locals.development ) {
+if (app.locals.development) {
   const webpack = require('webpack');
   const webpackConfig = require('../webpack.config');
   const webpackCompiler = webpack(webpackConfig);
 
-  app.use(require('webpack-dev-middleware')(webpackCompiler, {
-    noInfo: true,
-    publicPath: webpackConfig.output.publicPath,
-    stats: {
-      colors: true,
-      reasons: false,
-    }
-  }));
+  app.use(
+    require('webpack-dev-middleware')(webpackCompiler, {
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath,
+      stats: {
+        colors: true,
+        reasons: false,
+      },
+    })
+  );
 
-  app.use(require('webpack-hot-middleware')(webpackCompiler, {
-    path: '/__webpack_hmr',
-    heartbeat: 10 * 1000,
-  }));
+  app.use(
+    require('webpack-hot-middleware')(webpackCompiler, {
+      path: '/__webpack_hmr',
+      heartbeat: 10 * 1000,
+    })
+  );
 
   // Device type detection
   // On production we rely on Cloudfront to get this information for free
   const device = require('express-device');
   app.use(device.capture());
 
-  app.use('/img', proxy({target: 'http://cdn.lwjgl.org.s3.amazonaws.com', changeOrigin: true}));
-  app.use('/svg', proxy({target: 'http://cdn.lwjgl.org.s3.amazonaws.com', changeOrigin: true}));
+  // Proxy photos from S3
+  app.use('/img', proxy({ target: 'http://cdn.lwjgl.org.s3.amazonaws.com', changeOrigin: true }));
+  app.use('/svg', proxy({ target: 'http://cdn.lwjgl.org.s3.amazonaws.com', changeOrigin: true }));
 }
 
 // ------------------------------------------------------------------------------
 // Routing
 // ------------------------------------------------------------------------------
 
-app.use((req, res, next) =>
-{
-  if ( req.hostname === 'lwjgl.org' )
-  {
+app.use((req, res, next) => {
+  if (req.hostname === 'lwjgl.org') {
     res.redirect(301, `https://www.lwjgl.org${req.originalUrl}`);
     return;
   }
@@ -103,14 +106,16 @@ app.use((req, res, next) =>
 });
 
 // Static Assets
-app.use(express.static(path.join(__dirname, '../public'), {
-  index: false,
-  setHeaders: (res) => {
-    // Send immutable Cache-Control flag
-    // https://bitsup.blogspot.com/2016/05/cache-control-immutable.html
-    res.set('Cache-Control', 'public,max-age=31536000,immutable');
-  }
-}));
+app.use(
+  express.static(path.join(__dirname, '../public'), {
+    index: false,
+    setHeaders: res => {
+      // Send immutable Cache-Control flag
+      // https://bitsup.blogspot.com/2016/05/cache-control-immutable.html
+      res.set('Cache-Control', 'public,max-age=31536000,immutable');
+    },
+  })
+);
 
 // Teamcity proxy
 app.get('/teamcity', require('./teamcity'));
@@ -137,7 +142,7 @@ app.get('*', (req, res) => {
   let entry;
   let webpackManifest;
 
-  if ( app.locals.production ) {
+  if (app.locals.production) {
     // Set entry point
     entry = manifest.entry;
 
@@ -153,11 +158,11 @@ app.get('*', (req, res) => {
     // Append chunk of important routes to the preload list
     // Logic can be customized as needed. Can get complicated for recursive routes
     // or routes deep in site's hierarchy, so not always worth it
-    if ( req.path === '/' ) {
+    if (req.path === '/') {
       preloadScripts.push(manifest.routes.home);
     } else {
       const route = req.path.substr(1);
-      if ( manifest.routes[route] ) {
+      if (manifest.routes[route]) {
         preloadScripts.push(manifest.routes[route]);
       }
     }
@@ -170,7 +175,7 @@ app.get('*', (req, res) => {
   // These headers may be picked by supported CDNs or other reverse-proxies and push the assets via HTTP/2
   // To disable PUSH, append "; nopush"
   // More details: https://blog.cloudflare.com/announcing-support-for-http-2-server-push-2/
-  const linkHeaders = [...preloadScripts.map((script) => `\</js/${script}\>; rel=preload; as=script`)];
+  const linkHeaders = [...preloadScripts.map(script => `\</js/${script}\>; rel=preload; as=script`)];
 
   // Append Link headers
   res.set('Link', linkHeaders);
@@ -188,13 +193,13 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404);
 
-  if ( req.accepts('html', '*/*') === 'html' ) {
+  if (req.accepts('html', '*/*') === 'html') {
     res.render('404');
     return;
   }
 
-  if ( req.accepts('json', '*/*') === 'json' ) {
-    res.send({error: 'Not found'});
+  if (req.accepts('json', '*/*') === 'json') {
+    res.send({ error: 'Not found' });
     return;
   }
 
@@ -205,26 +210,26 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   res.status(500);
 
-  if ( req.accepts('html', '*/*') === 'html' ) {
+  if (req.accepts('html', '*/*') === 'html') {
     // HTML
-    res.render('500', {error: err});
-  } else if ( req.accepts('json', '*/*') === 'json' ) {
+    res.render('500', { error: err });
+  } else if (req.accepts('json', '*/*') === 'json') {
     // JSON
-    if ( err instanceof Error ) {
-      if ( req.app.locals.development ) {
+    if (err instanceof Error) {
+      if (req.app.locals.development) {
         const errorResponse = {};
 
         Object.getOwnPropertyNames(err).forEach(key => {
           errorResponse[key] = err[key];
         });
 
-        res.json({error: errorResponse});
+        res.json({ error: errorResponse });
       } else {
         // Only keep message in production because Error() may contain sensitive information
-        res.json({error: {message: err.message}});
+        res.json({ error: { message: err.message } });
       }
     } else {
-      res.send({error: err});
+      res.send({ error: err });
     }
   } else {
     // default to plain-text.
@@ -244,8 +249,8 @@ const server = app.listen(app.get('port'), () => {
   console.log(chalk.blue(`${PRODUCT} listening at http://${host}:${port}`));
 });
 
-server.on('error', (err) => {
-  if ( err.code === 'EADDRINUSE' ) {
+server.on('error', err => {
+  if (err.code === 'EADDRINUSE') {
     console.error(chalk.red(`${PRODUCT} address in use, exiting...`));
     process.exit(0);
   } else {
