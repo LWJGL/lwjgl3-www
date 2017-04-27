44d1ba7c57548ff49f7de6ffaa53b518659ef42c
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-04-27 20:05:23 +0000

    Revert to React 15.5.0

--- client/containers/Layout.js
+++ client/containers/Layout.js
@@ -8,20 +8,22 @@ import Footer from './Footer';
 import { Home, Download, Guide, Source, License } from '../routes';
 import Miss404 from '../routes/miss404';
 
-const Layout = () => [
-  <Helmet key="helmet" titleTemplate="%s - LWJGL" defaultTitle="LWJGL - Lightweight Java Game Library" />,
-  <Header key="header" />,
+const Layout = () => (
+  <div>
+    <Helmet titleTemplate="%s - LWJGL" defaultTitle="LWJGL - Lightweight Java Game Library" />
+    <Header key="header" />
 
-  <Switch key="main-switch">
-    <Route path="/" component={Home} exact={true} />
-    <Route path="/download" component={Download} exact={true} />
-    <Route path="/guide" component={Guide} exact={true} />
-    <Route path="/source" component={Source} exact={true} />
-    <Route path="/license" component={License} exact={true} />
-    <Route component={Miss404} />
-  </Switch>,
+    <Switch key="main-switch">
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/download" component={Download} exact={true} />
+      <Route path="/guide" component={Guide} exact={true} />
+      <Route path="/source" component={Source} exact={true} />
+      <Route path="/license" component={License} exact={true} />
+      <Route component={Miss404} />
+    </Switch>
 
-  <Footer key="footer" />,
-];
+    <Footer key="footer" />
+  </div>
+);
 
 export default Layout;
