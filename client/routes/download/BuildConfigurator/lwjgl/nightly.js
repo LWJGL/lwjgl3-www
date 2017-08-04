670016826e5655fe554f05000c0d6e5d7292c3dd
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-08-05 01:23:00 +0300

    feat: add rpmalloc to the nightly build

--- client/routes/download/BuildConfigurator/lwjgl/nightly.js
+++ client/routes/download/BuildConfigurator/lwjgl/nightly.js
@@ -1,5 +1,19 @@
-export default prev => ({
+import {
+  NATIVE_ALL
+} from '../constants'
+
+export default (prev) => ({
   ...prev,
   version: '3.1.3',
   alias: 'nightly',
-});
+  byId: {
+    ...prev.byId,
+    'lwjgl-rpmalloc': {
+      id: 'lwjgl-rpmalloc',
+      title: 'rpmalloc bindings',
+      description: 'A public domain cross platform lock free thread caching 16-byte aligned memory allocator implemented in C.',
+      natives: NATIVE_ALL,
+      website: 'https://github.com/rampantpixels/rpmalloc',
+    }
+  }
+})
