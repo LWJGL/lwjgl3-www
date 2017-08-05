6487c0fd4e5cb134870562908c516f26978b38aa
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-08-05 16:28:44 +0000

    feat: Add Flow defs for IntersectionObserver

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -17,3 +17,54 @@ declare var module: {
 declare var Intl: {
   NumberFormat: Function,
 };
+
+// TODO: Remove this when Flow adds support
+declare class DOMRectReadOnly {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  top: number,
+  right: number,
+  bottom: number,
+  left: number,
+  constructor(x: number, y: number, width: number, height: number): void,
+  static fromRect(rectangle?: { x?: number, y?: number, width?: number, height?: number }): DOMRect,
+}
+
+declare class DOMRect extends DOMRectReadOnly {
+  constructor(x: number, y: number, width: number, height: number): void,
+}
+
+// https://www.w3.org/TR/hr-time-2/#dom-domhighrestimestamp
+// https://developer.mozilla.org/en-US/docs/Web/API/DOMHighResTimeStamp
+declare type DOMHighResTimeStamp = number;
+
+declare type IntersectionObserverEntry = {
+  boundingClientRect: DOMRectReadOnly,
+  intersectionRatio: number,
+  intersectionRect: DOMRectReadOnly,
+  isIntersecting: boolean,
+  rootBounds: DOMRectReadOnly,
+  target: HTMLElement,
+  time: DOMHighResTimeStamp,
+};
+
+declare type IntersectionObserverCallback = (
+  entries: Array<IntersectionObserverEntry>,
+  observer: IntersectionObserver
+) => any;
+
+declare type IntersectionObserverOptions = {
+  root?: Node | null,
+  rootMargin?: string,
+  threshold?: number | Array<number>,
+};
+
+declare class IntersectionObserver {
+  constructor(callback: IntersectionObserverCallback, options?: IntersectionObserverOptions): void,
+  observe(target: HTMLElement): void,
+  unobserve(): void,
+  takeRecords(): Array<IntersectionObserverEntry>,
+  disconnect(): void,
+}
