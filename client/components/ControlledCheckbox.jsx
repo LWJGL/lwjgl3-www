b7e7a9aeca1be4c04761fd2fe92b55d085be56e7
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-10-01 22:05:52 +0000

    refactor: <Connect /> Toggle & Checkbox

--- client/components/ControlledCheckbox.jsx
+++ client/components/ControlledCheckbox.jsx
@@ -1,66 +1,41 @@
 // @flow
 import * as React from 'react';
-import { connect } from 'react-redux';
 import Checkbox from './Checkbox';
 import type { Dispatch } from 'redux';
-
-type Spec = {|
-  label: string,
-  action: (value: boolean) => {},
-  checked?: (state: any) => boolean,
-  disabled?: (state: any) => boolean,
-  hidden?: (state: any) => boolean,
-|};
-
-type OwnProps = {|
-  spec: Spec,
-|};
-
-type ConnectedProps = {|
-  label: string,
-  checked?: boolean,
-  disabled?: boolean,
-  hidden?: boolean,
-  handleClick: (value: boolean) => mixed,
-|};
+import Connect from '~/store/Connect';
 
 type Props = {|
-  ...OwnProps,
-  ...ConnectedProps,
+  spec: {|
+    label: string,
+    action: (value: boolean) => { type: string /* ... */ },
+    checked?: (state: any) => boolean,
+    disabled?: (state: any) => boolean,
+    hidden?: (state: any) => boolean,
+  |},
 |};
 
-class ControlledCheckbox extends React.Component<Props> {
-  toggle = () => {
-    this.props.handleClick(!this.props.checked);
-  };
-
-  render() {
-    const props: Props = this.props;
-
-    return (
-      <Checkbox
-        label={props.label}
-        disabled={props.disabled}
-        hidden={props.hidden}
-        checked={props.checked}
-        onChange={this.toggle}
-      />
-    );
-  }
-}
-
-export default connect(
-  (state: Object, ownProps: OwnProps) => {
-    const spec: Spec = ownProps.spec;
-
-    return {
+const ControlledCheckbox = ({ spec }: Props) => (
+  <Connect
+    state={state => ({
       label: spec.label,
       checked: spec.checked != null && spec.checked(state),
       disabled: spec.disabled != null && spec.disabled(state),
       hidden: spec.hidden != null && spec.hidden(state),
-    };
-  },
-  (dispatch: Dispatch<any>, ownProps: OwnProps) => ({
-    handleClick: (value: boolean) => dispatch(ownProps.spec.action(value)),
-  })
-)(ControlledCheckbox);
+    })}
+    actions={(dispatch: Dispatch<*>) => ({
+      handleClick: (value: boolean) => dispatch(spec.action(value)),
+    })}
+  >
+    {({ label, disabled, hidden, checked }, { handleClick }) => (
+      <Checkbox
+        label={label}
+        disabled={disabled}
+        hidden={hidden}
+        checked={checked}
+        onChange={handleClick.bind(this, !checked)}
+      />
+    )}
+  </Connect>
+);
+
+export default ControlledCheckbox;
--- client/components/ControlledToggle.jsx
+++ client/components/ControlledToggle.jsx
@@ -1,55 +1,41 @@
 // @flow
 import * as React from 'react';
-import { connect } from 'react-redux';
 import Toggle from './Toggle';
 import type { Dispatch } from 'redux';
+import Connect from '~/store/Connect';
 
-type OwnProps = {|
+type Props = {|
   spec: {|
     label: string,
-    action: (value: boolean) => { type: 'string' /* ... */ },
+    action: (value: boolean) => { type: string /* ... */ },
     checked?: (state: any) => boolean,
     disabled?: (state: any) => boolean,
     hidden?: (state: any) => boolean,
   |},
 |};
 
-type ConnectedProps = {|
-  label: string,
-  checked?: boolean,
-  disabled?: boolean,
-  hidden?: boolean,
-  handleClick: (value: any) => mixed,
-|};
-
-type Props = {|
-  ...OwnProps,
-  ...ConnectedProps,
-|};
-
-class ControlledToggle extends React.Component<Props> {
-  toggle = () => {
-    this.props.handleClick(!this.props.checked);
-  };
-
-  render() {
-    const { label, disabled, hidden, checked } = this.props;
-    return <Toggle label={label} disabled={disabled} hidden={hidden} checked={checked} onChange={this.toggle} />;
-  }
-}
-
-export default connect(
-  (state: any, ownProps: OwnProps) => {
-    const spec = ownProps.spec;
-
-    return {
-      checked: spec.checked && spec.checked(state),
-      disabled: spec.disabled && spec.disabled(state),
-      hidden: spec.hidden && spec.hidden(state),
+const ControlledToggle = ({ spec }: Props) => (
+  <Connect
+    state={state => ({
       label: spec.label,
-    };
-  },
-  (dispatch: Dispatch<*>, ownProps: OwnProps) => ({
-    handleClick: value => dispatch(ownProps.spec.action(value)),
-  })
-)(ControlledToggle);
+      checked: spec.checked != null && spec.checked(state),
+      disabled: spec.disabled != null && spec.disabled(state),
+      hidden: spec.hidden != null && spec.hidden(state),
+    })}
+    actions={(dispatch: Dispatch<*>) => ({
+      handleClick: (value: boolean) => dispatch(spec.action(value)),
+    })}
+  >
+    {({ label, disabled, hidden, checked }, { handleClick }) => (
+      <Toggle
+        label={label}
+        disabled={disabled}
+        hidden={hidden}
+        checked={checked}
+        onChange={handleClick.bind(this, !checked)}
+      />
+    )}
+  </Connect>
+);
+
+export default ControlledToggle;
