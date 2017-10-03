460ca108dfe54315b5018c63c698dce7c08234aa
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-10-03 16:15:05 +0000

    fix: nest descendant subscriptions, fire when !sCU
    We nest subscriptions of descendant components, similar to what
    react-redux is doing in ./src/utils/Subscription.js, so that we can
    ensure the ancestor components re-render before descendants.
    
    This avoid bugs like removing a TODO item from the store while the
    component rendering that TODO is still mounted. Previous implementation
    fired TODO's listener before the ancestor had the chance to unmount it.

--- client/store/Connect.jsx
+++ client/store/Connect.jsx
@@ -1,7 +1,10 @@
 // @flow
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { bindActionCreators, type ActionCreator, type ActionCreators, type Dispatch, type DispatchAPI } from 'redux';
 import store from './';
+//$FlowFixMe
+import shallowEqual from 'fbjs/lib/shallowEqual';
 
 type Actions = {
   dispatch: Dispatch<*>,
@@ -13,17 +16,50 @@ type mapDispatchToProps = ActionCreators<*, *> | ((dispatch: Dispatch<*>) => Act
 type Props<S> = {
   state: (state: Object) => S,
   actions?: mapDispatchToProps,
-  children: (state: S, actions: Actions) => React.Node,
+  render?: (state: S, actions: Actions) => React.Node,
+  children?: (state: S, actions: Actions) => React.Node,
 };
 
-class Connect extends React.PureComponent<Props<*>, Object> {
-  unsubscribe: () => void;
+type Context = {
+  parent: Connect,
+};
+
+class Connect extends React.Component<Props<*>, *> {
+  // Nest subscriptions of descendant components, so that we can
+  // ensure the ancestor components re-render before descendants.
+  listeners = [];
+
+  // Only top-level <Connect /> needs to subscribe to the store
+  storeUnsubscribe: Function;
+
+  // Cache mapDispatchToProps, re-compute only when props change
   actions: Actions;
 
+  // Parent will be undefined for top-level <Connect />
+  static contextTypes = {
+    parent: PropTypes.object,
+  };
+
+  // Provide this <Connect /> to descendants
+  static childContextTypes = {
+    parent: PropTypes.object,
+  };
+
+  getChildContext(): Context {
+    return {
+      parent: this,
+    };
+  }
+
+  // Fired when redux state changes
   storeListener = () => {
     this.setState(this.props.state(store.getState()));
   };
 
+  // Wraps action creators with dispatch
+  // A. Supports passing a function, first argument is the store's dispatch
+  // B. Supports passing an object whose values are action creators
+  // @see http://redux.js.org/docs/api/bindActionCreators.html
   mapDispatch(actions: mapDispatchToProps): Actions {
     if (typeof actions === 'function') {
       return actions(store.dispatch);
@@ -32,10 +68,35 @@ class Connect extends React.PureComponent<Props<*>, Object> {
     return bindActionCreators(actions, store.dispatch);
   }
 
-  constructor(props: Props<*>) {
+  constructor(props: Props<*>, context: Context) {
     super(props);
+
+    // Map action creators and cache them to actions
     this.actions = { dispatch: store.dispatch, ...(this.props.actions && this.mapDispatch(this.props.actions)) };
+
+    // Compute initial state
     this.state = this.props.state(store.getState());
+
+    if (context.parent === undefined) {
+      // This is a top-level Connect, subscribe to the store directly
+      this.storeUnsubscribe = store.subscribe(this.storeListener);
+    } else {
+      // This is a descendant Connect, subscribe to its parent
+      context.parent.subscribe(this.storeListener);
+    }
+  }
+
+  shouldComponentUpdate(nextProps: Props<*>, nextState: *) {
+    // Shallow comparison of props & state, make sure your store is immutable
+    const isEqual = shallowEqual(this.props, nextProps) && shallowEqual(this.state, nextState);
+
+    // When true the descendants will have the chance to run their own sCU and re-render,
+    // so call their listeners only sCU returns false
+    if (isEqual) {
+      this.notify();
+    }
+
+    return !isEqual;
   }
 
   componentWillReceiveProps(nextProps: Props<*>) {
@@ -43,16 +104,37 @@ class Connect extends React.PureComponent<Props<*>, Object> {
     this.setState(nextProps.state(store.getState()));
   }
 
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(this.storeListener);
+  componentWillUnmount() {
+    if (this.context.parent === undefined) {
+      this.storeUnsubscribe();
+    } else {
+      this.context.parent.unsubscribe(this.storeListener);
+    }
   }
 
-  componentWillUnmount() {
-    this.unsubscribe();
+  subscribe(listener: typeof Connect.prototype.storeListener) {
+    this.listeners.push(listener);
+  }
+
+  unsubscribe(listener: typeof Connect.prototype.storeListener) {
+    this.listeners.splice(this.listeners.indexOf(listener), 1);
+  }
+
+  notify() {
+    this.listeners.forEach(listener => {
+      listener();
+    });
   }
 
   render() {
-    return this.props.children(this.state, this.actions);
+    if (this.props.render !== undefined) {
+      // render prop, takes precedence
+      return this.props.render(this.state, this.actions);
+    } else if (this.props.children !== undefined) {
+      // function-as-children
+      return this.props.children(this.state, this.actions);
+    }
+    return null;
   }
 }
 
