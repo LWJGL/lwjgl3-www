3a6c04e24e988931004471e668edea6947dfa977
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-10-04 00:09:09 +0300

    feat: add Remotery to the nightly build

--- client/routes/customize/BuildConfigurator/lwjgl/nightly.js
+++ client/routes/customize/BuildConfigurator/lwjgl/nightly.js
@@ -5,4 +5,15 @@ export default (prev: BuildOptions): BuildOptions => ({
   ...prev,
   version: '3.1.4',
   alias: 'nightly',
+  byId: {
+    ...prev.byId,
+    'lwjgl-remotery': {
+      id: 'lwjgl-remotery',
+      title: 'Remotery bindings',
+      description:
+        'A realtime CPU/GPU profiler hosted in a single C file with a viewer that runs in a web browser.',
+      natives: NATIVE_ALL,
+      website: 'https://github.com/Celtoys/Remotery',
+    },
+  },
 });
