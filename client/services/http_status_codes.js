d226dc607477ee3c389fe179183e903e239409c6
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-10-04 17:09:22 +0000

    refactor: use flow constants for HTTP status codes

--- client/services/http_status_codes.js
+++ client/services/http_status_codes.js
@@ -1,12 +1,12 @@
 // @flow
-export const HTTP_OK: number = 200;
-// export const HTTP_CREATED: number = 201;
-// export const HTTP_ACCEPTED: number = 202;
-// export const HTTP_NO_CONTENT: number = 204;
-// export const HTTP_MOVED_PERMANENTLY: number = 301;
-// export const HTTP_BAD_REQUEST: number = 400;
-// export const HTTP_UNAUTHORIZED: number = 401;
-// export const HTTP_FORBIDDEN: number = 403;
-// export const HTTP_NOT_FOUND: number = 404;
-// export const HTTP_METHOD_NOT_ALLOWED: number = 405;
-// export const HTTP_INTERNAL_SERVER_ERROR: number = 500;
+export const HTTP_OK: 200 = 200;
+// export const HTTP_CREATED: 201 = 201;
+// export const HTTP_ACCEPTED: 202 = 202;
+// export const HTTP_NO_CONTENT: 204 = 204;
+// export const HTTP_MOVED_PERMANENTLY: 301 = 301;
+// export const HTTP_BAD_REQUEST: 400 = 400;
+// export const HTTP_UNAUTHORIZED: 401 = 401;
+// export const HTTP_FORBIDDEN: 403 = 403;
+// export const HTTP_NOT_FOUND: 404 = 404;
+// export const HTTP_METHOD_NOT_ALLOWED: 405 = 405;
+// export const HTTP_INTERNAL_SERVER_ERROR: 500 = 500;
