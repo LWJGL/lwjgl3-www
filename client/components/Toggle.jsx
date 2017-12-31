bbc1b7a0558c76771f99e3b02328b0a933ce4f1c
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2017-12-31 01:22:55 +0000

    feat: use styled components in <Toggle />

--- client/components/Toggle.jsx
+++ client/components/Toggle.jsx
@@ -1,6 +1,37 @@
 // @flow
 import * as React from 'react';
 import { uniqueId } from 'lodash-es';
+import styled from 'react-emotion';
+import { COLOR_WHITE, COLOR_CUSTOM_CONTROL_INDICATOR_BG } from '~/theme';
+
+const Label = styled('label')`
+  &:before {
+    width: 1.5rem;
+    left: -0.25rem;
+    background: ${COLOR_CUSTOM_CONTROL_INDICATOR_BG.darken(10).css()};
+  }
+
+  &:after {
+    width: 0.8rem;
+    height: 0.8rem;
+    top: 0;
+    left: 0;
+    transform: translate(-0.13rem, 0.345rem);
+    background-color: ${COLOR_WHITE.css()};
+  }
+
+  &:after,
+  &:before {
+    border-radius: 1rem;
+    transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+`;
+
+const Input = styled('input')`
+  &:checked ~ ${Label}:after {
+    transform: translate(0.35rem, 0.345rem);
+  }
+`;
 
 type Props = {
   label: string,
@@ -36,18 +67,18 @@ export class Toggle extends React.PureComponent<Props> {
     const props = this.props;
 
     return props.hidden === true ? null : (
-      <div className="custom-control custom-toggle">
-        <input
+      <div className="custom-control">
+        <Input
+          className="custom-control-input"
           type="checkbox"
           id={this.htmlForId}
-          className="custom-control-input"
           disabled={props.disabled}
           checked={props.checked}
           onChange={this.change}
         />
-        <label className="custom-control-label" htmlFor={this.htmlForId}>
+        <Label className="custom-control-label" htmlFor={this.htmlForId}>
           {props.label}
-        </label>
+        </Label>
       </div>
     );
   }
