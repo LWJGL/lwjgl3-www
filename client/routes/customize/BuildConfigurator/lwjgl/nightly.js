36bc7cf2ca19ab9637f59028abee04bd8aaa38d7
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-02-14 14:05:40 +0200

    feat: start LWJGL 3.1.7

--- client/routes/customize/BuildConfigurator/lwjgl/nightly.js
+++ client/routes/customize/BuildConfigurator/lwjgl/nightly.js
@@ -1,12 +1,25 @@
 // @flow
-import { NATIVE_ALL } from '../constants';
+import { NATIVE_MAC, NATIVE_ALL } from '../constants';
 import type { BuildOptions } from '../types';
 
 export default (prev: BuildOptions): BuildOptions => ({
   ...prev,
-  version: '3.1.6',
+  version: '3.1.7',
   alias: 'nightly',
   byId: {
     ...prev.byId,
+    'lwjgl-vma': {
+      id: 'lwjgl-vma',
+      title: 'Vulkan Memory Allocator',
+      description:
+        'An easy to integrate Vulkan memory allocation library.',
+      natives: NATIVE_ALL,
+      website: 'https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator',
+    },
+    'lwjgl-vulkan': {
+      ...prev.byId['lwjgl-vulkan'],
+      natives: [NATIVE_MAC],
+      nativesOptional: true
+    },
   },
 });
