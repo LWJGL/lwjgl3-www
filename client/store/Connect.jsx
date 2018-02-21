7c2277b1bd2b7759f5c6f23db92aa40ed6043fc5
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-02-21 08:37:40 +0000

    feat: Fiber <Connect />
    - enforce subscription of ancestors first
    - ancestors force updates to descendants
    - uses React.createContext()
    - no longer using deprecated lifecycle methods
    - removed listeners logic
    - no longer supports updating props at runtime

--- client/store/Connect.jsx
+++ client/store/Connect.jsx
@@ -1,9 +1,20 @@
 // @flow
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { bindActionCreators, type ActionCreator, type ActionCreators, type Dispatch, type DispatchAPI } from 'redux';
 import { store } from './';
 import shallowEqual from 'fbjs/lib/shallowEqual';
+
+// Wraps action creators with dispatch
+// A. Supports passing a function, first argument is the store's dispatch
+// B. Supports passing an object whose values are action creators
+// @see http://redux.js.org/docs/api/bindActionCreators.html
+function mapDispatch(actions: mapDispatchToProps): Actions {
+  if (typeof actions === 'function') {
+    return actions(store.dispatch);
+  }
+
+  return bindActionCreators(actions, store.dispatch);
+}
 
 type Actions = {
   dispatch: Dispatch<*>,
@@ -18,115 +29,85 @@ type Props<S> = {
   children: (state: S, actions: Actions) => React.Node,
 };
 
-type Context = {
-  connectParent: Connect,
+type PropsSlice<S> = {
+  state: (state: Object) => S,
+  actions?: mapDispatchToProps,
+  children: (state: S, actions: Actions) => React.Node,
+  ancestor?: ConnectSlice,
 };
 
-export class Connect extends React.Component<Props<*>, *> {
-  // Parent will be undefined for top-level <Connect />
-  static contextTypes = {
-    connectParent: PropTypes.object,
-  };
+/*
+  Use Context to enforce subscription order:
+  Since cDM runs first for descendants, we need a mechanism
+  to ensure ancestors subscribe first
+*/
+//$FlowFixMe
+const ConnectContext: Context<ConnectSlice> = React.createContext();
 
-  // Provide this <Connect /> to descendants
-  static childContextTypes = {
-    connectParent: PropTypes.object,
-  };
+export const Connect = (props: Props<*>) => (
+  // Ancestor will be undefined for top level Connect
+  <ConnectContext.Consumer>{ancestor => <ConnectSlice ancestor={ancestor} {...props} />}</ConnectContext.Consumer>
+);
 
-  getChildContext(): Context {
-    return {
-      connectParent: this,
-    };
-  }
-
-  // Fired when redux state changes
-  storeListener = this.storeListener.bind(this);
-  storeListener() {
-    this.setState(this.props.state(store.getState()));
-  }
-
-  // Wraps action creators with dispatch
-  // A. Supports passing a function, first argument is the store's dispatch
-  // B. Supports passing an object whose values are action creators
-  // @see http://redux.js.org/docs/api/bindActionCreators.html
-  mapDispatch(actions: mapDispatchToProps): Actions {
-    if (typeof actions === 'function') {
-      return actions(store.dispatch);
-    }
-
-    return bindActionCreators(actions, store.dispatch);
-  }
-
-  // Nest subscriptions of descendant components, so that we can
-  // ensure the ancestor components re-render before descendants.
-  listeners = [];
-
-  // Only top-level <Connect /> needs to subscribe to the store
-  storeUnsubscribe: Function;
-
-  // Cache mapDispatchToProps, re-compute only when props change
+class ConnectSlice extends React.Component<PropsSlice<*>, *> {
+  // Cache actions
+  // TODO: Should we support runtime modification of actions/state props?
   actions: Actions = {
     dispatch: store.dispatch,
-    ...(this.props.actions && this.mapDispatch(this.props.actions)),
+    ...(this.props.actions && mapDispatch(this.props.actions)),
   };
 
   // Compute initial state
   state = this.props.state(store.getState());
 
-  UNSAFE_componentWillMount() {
-    if (this.context.connectParent === undefined) {
-      // This is a top-level Connect, subscribe to the store directly
-      this.storeUnsubscribe = store.subscribe(this.storeListener);
-    } else {
-      // This is a descendant Connect, subscribe to its parent
-      this.context.connectParent.subscribe(this.storeListener);
+  unsubscribe: Function | null = null;
+  shouldUpdate: boolean = false;
+
+  subscribe() {
+    if (this.unsubscribe === null) {
+      if (this.props.ancestor) {
+        // Subscribe ancestors first
+        this.props.ancestor.subscribe();
+      }
+      this.unsubscribe = store.subscribe(this.onStoreStateChange);
     }
+  }
+
+  componentDidMount() {
+    this.subscribe();
   }
 
   componentWillUnmount() {
-    if (this.context.connectParent === undefined) {
-      this.storeUnsubscribe();
-    } else {
-      this.context.connectParent.unsubscribe(this.storeListener);
+    if (this.unsubscribe !== null) {
+      this.unsubscribe();
     }
   }
 
-  shouldComponentUpdate(nextProps: Props<*>, nextState: *) {
-    // Shallow comparison of props & state, make sure your store is immutable
-    const isEqual = shallowEqual(this.props, nextProps) && shallowEqual(this.state, nextState);
-
-    // When true the descendants will have the chance to run their own sCU and re-render,
-    // so call their listeners only sCU returns false
-    if (isEqual) {
-      this.notify();
-    }
-
-    return !isEqual;
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps: Props<*>) {
-    this.actions = {
-      dispatch: store.dispatch,
-      ...(nextProps.actions && this.mapDispatch(nextProps.actions)),
-    };
-    this.setState(nextProps.state(store.getState()));
-  }
-
-  subscribe(listener: typeof Connect.prototype.storeListener) {
-    this.listeners.push(listener);
-  }
-
-  unsubscribe(listener: typeof Connect.prototype.storeListener) {
-    this.listeners.splice(this.listeners.indexOf(listener), 1);
-  }
-
-  notify() {
-    this.listeners.forEach(listener => {
-      listener();
+  // Fired when redux state changes
+  onStoreStateChange = this.onStoreStateChange.bind(this);
+  onStoreStateChange() {
+    this.setState((prevState, props) => {
+      const nextState = props.state(store.getState());
+      // Force update if ancestor <Connect /> had an update.
+      // This is required because children of Slice descendants may
+      // require a prop from an ancestor. e.g.:
+      // AncestorConnect => SomeProps => DescendantConnect => ChildDependsOn<SomeProps>
+      this.shouldUpdate = (props.ancestor && props.ancestor.shouldUpdate) || !shallowEqual(prevState, nextState);
+      return this.shouldUpdate ? nextState : null;
     });
   }
 
+  shouldComponentUpdate() {
+    return this.shouldUpdate;
+  }
+
+  componentDidUpdate() {
+    this.shouldUpdate = false;
+  }
+
   render() {
-    return this.props.children(this.state, this.actions);
+    return (
+      <ConnectContext.Provider value={this}>{this.props.children(this.state, this.actions)}</ConnectContext.Provider>
+    );
   }
 }
