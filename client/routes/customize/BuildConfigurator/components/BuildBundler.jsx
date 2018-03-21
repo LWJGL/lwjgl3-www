0ab090aca284360d154267cf00231278efe3a0e0
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-03-21 15:02:54 +0200

    fix: broken bundler layout in Edge

--- client/routes/customize/BuildConfigurator/components/BuildBundler.jsx
+++ client/routes/customize/BuildConfigurator/components/BuildBundler.jsx
@@ -11,9 +11,9 @@ type Props = {
 export class BuildBundler extends React.Component<Props> {
   render() {
     return (
-      <div className="col text-left">
+      <div className="col py-3">
         <div className="text-center">
-          <LoaderSpinner size={32} className="my-3" />
+          <LoaderSpinner size={32} className="my-3" style={{ stroke: 'white' }} />
           <h4>Generating ZIP bundle. Please wait...</h4>
           <p>
             <button className="btn btn-sm btn-danger" onClick={this.props.cancel}>
