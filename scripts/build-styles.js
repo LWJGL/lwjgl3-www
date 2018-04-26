a9ffd2e92dbd3fc7bf03c6c15a2e1c973664f7e7
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-04-26 15:29:01 +0300

    build: async file writes in "build styles" + upgrade deps

--- scripts/build-styles.js
+++ scripts/build-styles.js
@@ -5,6 +5,8 @@ const postcss = require('postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const { argv } = require('yargs');
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 /*
   1. Render Sass to CSS
@@ -19,42 +21,42 @@ const { argv } = require('yargs');
     --sourcemap generates source map
 */
 
-const sourcePath = path.resolve(__dirname, '../client/styles/layout.scss');
-const targetPath = path.resolve(__dirname, '../public/css/core.css');
-const sourceMapPath = path.resolve(__dirname, '../public/css/core.css.map');
-const SOURCEMAP = argv.sourcemap === true;
+const process = async () => {
+  const sourcePath = path.resolve(__dirname, '../client/styles/layout.scss');
+  const targetPath = path.resolve(__dirname, '../public/css/core.css');
+  const sourceMapPath = path.resolve(__dirname, '../public/css/core.css.map');
+  const SOURCEMAP = argv.sourcemap === true;
 
-console.log('Rendering Sass to CSS');
+  console.log('Rendering Sass to CSS');
 
-const result = sass.renderSync({
-  file: sourcePath,
-  outputStyle: 'expanded',
-  precision: 6,
-  sourceComments: false,
-  sourceMap: SOURCEMAP,
-  sourceMapEmbed: SOURCEMAP,
-  sourceMapRoot: '/',
-});
+  const render = await util.promisify(sass.render)({
+    file: sourcePath,
+    outputStyle: 'expanded',
+    precision: 6,
+    sourceComments: false,
+    sourceMap: SOURCEMAP,
+    sourceMapEmbed: SOURCEMAP,
+    sourceMapRoot: '/',
+  });
 
-console.log('Applying Autoprefixer & Minifying CSS');
+  console.log('Applying Autoprefixer & Minifying CSS');
 
-postcss([
-  autoprefixer,
-  cssnano({
-    preset: [
-      'default',
-      {
-        // colormin: false,
-        calc: false,
-        zindex: false,
-        discardComments: {
-          removeAll: true,
+  const result = await postcss([
+    autoprefixer,
+    cssnano({
+      preset: [
+        'default',
+        {
+          // colormin: false,
+          calc: false,
+          zindex: false,
+          discardComments: {
+            removeAll: true,
+          },
         },
-      },
-    ],
-  }),
-])
-  .process(result.css, {
+      ],
+    }),
+  ]).process(render.css, {
     from: sourcePath,
     to: targetPath,
     map: SOURCEMAP
@@ -62,10 +64,12 @@ postcss([
           inline: false,
         }
       : false,
-  })
-  .then(result => {
-    fs.writeFile(targetPath, result.css);
-    if (result.map) {
-      fs.writeFile(sourceMapPath, result.map);
-    }
   });
+
+  await writeFileAsync(targetPath, result.css);
+  if (result.map) {
+    await writeFileAsync(sourceMapPath, result.map);
+  }
+};
+
+process();
