fddb7c980597f569df52e5d112ef6582a08b80e1
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-05-06 18:23:53 +0300

    refactor: svg icon styles with @emotion

--- client/components/icons/Icon.jsx
+++ client/components/icons/Icon.jsx
@@ -1,5 +1,8 @@
 // @flow
+// @jsx jsx
 import * as React from 'react';
+import { jsx } from '@emotion/core';
+import css from '@emotion/css';
 
 export type Props = {
   style?: { ...CSSStyleDeclaration },
@@ -7,8 +10,26 @@ export type Props = {
   props?: { [$Keys<HTMLElement>]: any },
 };
 
+// Based on https://blog.prototypr.io/align-svg-icons-to-text-and-say-goodbye-to-font-icons-d44b3d7b26b4#.58pqpyl6w
+const SvgIcon = css`
+  display: inline-flex;
+  align-self: center;
+  position: relative;
+  height: 1em;
+  width: 1em;
+  svg {
+    height: 1em;
+    width: 1em;
+    bottom: -0.125em;
+    position: absolute;
+    .align-items-center & {
+      bottom: auto;
+    }
+  }
+`;
+
 export const Icon = ({ children, style, ...props }: Props) => (
-  <span className="svg-icon" style={style}>
+  <span css={SvgIcon} className="svg-icon" style={style}>
     <svg children={children} fill="currentColor" preserveAspectRatio="xMidYMid meet" {...props} />
   </span>
 );
--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -5,6 +5,7 @@ import { Layout } from './Layout';
 import { BreakpointProvider } from '../components/Breakpoint';
 
 // Pull common modules that we want preloaded in the main chunk
+import '../components/icons/Icon';
 import './PageView';
 
 const supportsHistory = 'pushState' in window.history;
