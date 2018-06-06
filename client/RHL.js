098c865a1487a199c2869ef34e3ae51426505ea2
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-06-06 15:01:40 +0300

    feat: RHL now marks node_modules/* as cold

--- client/RHL.js
+++ client/RHL.js
@@ -1,3 +1,8 @@
-import { hot } from 'react-hot-loader';
+import { setConfig, cold, hot } from 'react-hot-loader';
 import App from './containers/App';
+
+setConfig({
+  onComponentRegister: (type, name, file) => file.indexOf('node_modules') > 0 && cold(type),
+});
+
 export default hot(module)(App);
