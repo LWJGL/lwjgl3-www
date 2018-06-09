006eeb961d1b2dd8d212e24f6a061c4a593b4f19
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-06-09 13:46:43 +0300

    feat: Limit scroll restoration entries to 50
    Use Map() to avoid expensive array operations.
    
    Map() provides fast random lookups and FIFO properties since
    keys() are always returned in order. Therefore by iterating just one
    time we can get oldest item's key and remove it from the Map()
    
    We set max entries to 50 for the sole reason that this is the default
    for Firefox (couldn't find info on other browsers).

--- client/containers/PageView.jsx
+++ client/containers/PageView.jsx
@@ -2,12 +2,19 @@
 import * as React from 'react';
 import { ErrorBoundary } from '../components/ErrorBoundary';
 import { PageError } from '../components/PageError';
-
 import type { RouterLocation } from '@reach/router';
 
-type ScrollPositions = {
-  [key: string]: Array<number>,
+// Store scroll position when leaving a route, restore if we return back to it
+
+type ScrollPosition = {
+  x: number,
+  y: number,
 };
+
+// The maximum number of pages in the browser's session history, i.e. the maximum number of URLs you can traverse purely through the Back/Forward buttons.
+// Default value is 50 to match Firefox's default value (about:config -> browser.sessionhistory.max_entries)
+const MAX_SCROLL_ENTRIES = 50;
+const scrollEntries: Map<string, ScrollPosition> = new Map();
 
 type Props = {
   location: RouterLocation,
@@ -15,27 +22,20 @@ type Props = {
 };
 
 export class PageView extends React.Component<Props> {
-  // Remembers last scroll positions
-  // TODO: We shouldn't allow infinite keys here, implement a *shared* LRU cache
-  static scrollPositions: ScrollPositions = {};
-
   componentDidMount() {
     const {
       location: { key = 'root', pathname, search },
     } = this.props;
-    let scrollToTop: boolean = this.props.location.hash.length === 0;
 
     // Track in Google Analytics
     window.ga('send', 'pageview', `${pathname}${search}`);
 
     // If we have previously stored the same key, restore scroll position
-    const pos = PageView.scrollPositions[key];
-    if (pos !== undefined) {
-      window.scroll(pos[0], pos[1]);
-      scrollToTop = false;
-    }
-
-    if (scrollToTop) {
+    const entry = scrollEntries.get(key);
+    if (entry !== undefined) {
+      // Restore scroll position
+      window.scroll(entry.x, entry.y);
+    } else if (this.props.location.hash.length === 0) {
       // Scroll to top of viewport
       window.scroll(0, 0);
     }
@@ -43,17 +43,27 @@ export class PageView extends React.Component<Props> {
 
   componentWillUnmount() {
     // Remember scroll position so we can restore if we return to this view via browser history
+    // * [window.scrollX, window.scrollY];  // <-- Only supported in CSSOM browsers
+    // * [window.pageXOffset, window.pageYOffset]; // IE9+ pageXOffset is alias of scrollX
     const {
       location: { key = 'root' },
     } = this.props;
-    // RouteModule.scrollPositions[key] = [window.scrollX, window.scrollY];  // <-- Only supported in CSSOM browsers
-    PageView.scrollPositions[key] = [window.pageXOffset, window.pageYOffset]; // IE9+ pageXOffset is alias of scrollX
+
+    // Add entry
+    scrollEntries.set(key, { x: window.pageXOffset, y: window.pageYOffset });
+
+    // Drop oldest entry if we exceeded maximum number of entries
+    if (scrollEntries.size > MAX_SCROLL_ENTRIES) {
+      // * The keys() method returns a new Iterator object that contains the keys for each element in the Map object in **insertion** order.
+      // * Therefore, the first value returned by the Iterator will be the oldest scroll entry that we need to drop.
+      //$FlowFixMe
+      scrollEntries.delete(scrollEntries.keys().next().value);
+    }
+
+    // console.table(Array.from(scrollEntries.keys()));
   }
 
   render() {
-    // TODO: Use error boundary but find a way to re-render on hot reload
-    // TODO: Custom rendering of errors
-    // TODO: Steal ideas from create-react-app's react-error-overlay (e.g. open file in editor)
     if (!FLAG_PRODUCTION) {
       // return <React.StrictMode>{this.props.children}</React.StrictMode>;
       return this.props.children;
