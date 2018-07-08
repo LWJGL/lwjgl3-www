2bd3b18b9a99046dac706d616c5cf831f1608dea
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-07-08 19:26:17 +0300

    flow: remove obsolete def

--- flow-typed/custom/react-emotion.js
+++
@@ -1,4 +0,0 @@
-declare module 'react-emotion' {
-  declare type StyledOptions = { [string]: ?string };
-  declare export default (tag: string, options: ?StyledOptions) => (css: Array<string>) => React.ElementType;
-}
