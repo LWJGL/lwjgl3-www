2a3b5955c3309ce57cf6e2022622cd63bdc78bbc
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-08-06 12:48:27 +0300

    feat: start LWJGL 3.2.1

--- client/routes/customize/BuildConfigurator/lwjgl/nightly.js
+++ client/routes/customize/BuildConfigurator/lwjgl/nightly.js
@@ -4,6 +4,6 @@ import type { BuildOptions } from '../types';
 
 export default (prev: BuildOptions): BuildOptions => ({
   ...prev,
-  version: '3.2.0',
+  version: '3.2.1',
   alias: 'nightly',
 });
