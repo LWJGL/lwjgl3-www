ae6330e0426fd97379d5c90947803892bb12f09b
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-08-14 02:49:26 +0300

    build: Pull common modules on main bundle

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -4,6 +4,12 @@ import { Layout } from './Layout';
 import { BreakpointProvider } from '../components/Breakpoint';
 import { ServiceWorkerProvider } from '../components/ServiceWorker';
 
+// Pull common modules on main bundle
+import './PageView';
+import '../components/Head';
+import '../components/Title';
+import '../components/HashLinkTarget';
+
 const App = () => (
   <ServiceWorkerProvider>
     <BreakpointProvider>
