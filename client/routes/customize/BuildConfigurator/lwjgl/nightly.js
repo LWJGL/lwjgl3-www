1af673a10404e94db0e51a981e1562c39c872692
Leon Linhart <themrmilchmann@gmail.com>
2018-08-25 20:22:10 +0200

    feat: add Opus to the nightly build

--- client/routes/customize/BuildConfigurator/lwjgl/nightly.js
+++ client/routes/customize/BuildConfigurator/lwjgl/nightly.js
@@ -6,4 +6,14 @@ export default (prev: BuildOptions): BuildOptions => ({
   ...prev,
   version: '3.2.1',
   alias: 'nightly',
+  byId: {
+    ...prev.byId,
+    'lwjgl-opus': {
+      id: 'lwjgl-opus',
+      title: 'Opus bindings',
+      description: 'Opus is a totally open, royalty-free, highly versatile audio codec.',
+      natives: NATIVE_ALL,
+      website: 'https://opus-codec.org/',
+    },
+  },
 });
