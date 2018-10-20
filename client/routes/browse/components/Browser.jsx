b1dd97e9314c8048d69fe83c5b56ec1ab4ffc0af
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-20 03:47:25 +0300

    refactor: implement <Browser /> with react-cache + suspense

--- client/routes/browse/components/Browser.jsx
+++ client/routes/browse/components/Browser.jsx
@@ -1,6 +1,10 @@
 // @flow
 // @jsx jsx
 import * as React from 'react';
+//$FlowFixMe
+import { unstable_Suspense as Suspense, Fragment } from 'react';
+import { createCache, createResource } from 'react-cache';
+
 import { jsx } from '@emotion/core';
 import { Folder, FolderTH } from './Folder';
 import { File } from './File';
@@ -8,8 +12,11 @@ import IconCloud from '~/components/icons/md/Cloud';
 import IconChevronRight from '~/components/icons/md/ChevronRight';
 import { Link } from '@reach/router';
 import { CircularProgress } from '~/components/CircularProgress';
-
 import { HTTP_OK } from '~/services/http_status_codes';
+
+type Props = {
+  path: string,
+};
 
 type FolderData = {|
   path: string,
@@ -17,39 +24,88 @@ type FolderData = {|
   folders: Array<string>,
 |};
 
-type ContentCache = {
-  [path: string]: FolderData,
-};
+const cache = createCache();
+const BrowserContentsResource = createResource(fetchContents);
 
 async function fetchContents(path: string): Promise<FolderData> {
-  const response = await fetch(`/list?path=${path}`);
+  if (path === '') {
+    return Promise.resolve({
+      path: '',
+      files: [],
+      folders: ['release', 'stable', 'nightly'],
+    });
+  }
+
+  const response = await fetch(`/list?path=${path}/`);
   if (response.status !== HTTP_OK) {
     throw response.statusText;
   }
-  return await response.json();
+
+  let contents = await response.json();
+  contents.path = path;
+  if (contents.files === undefined) {
+    contents.files = [];
+  }
+  contents.folders =
+    contents.folders === undefined ? [] : contents.folders.map(name => name.substring(0, name.length - 1));
+
+  return contents;
+}
+
+export function Browser({ path }: Props) {
+  return (
+    <div className="table-responsive-md mt-sm-4">
+      <table className="table mb-0">
+        <thead className="thead-light">
+          <tr>
+            <th colSpan={2}>
+              <IconCloud /> &nbsp;
+              <Link to={'/browse'}>lwjgl</Link>
+              {path.length
+                ? path.split('/').map((it, i, arr) => {
+                    const subpath = arr.slice(0, i + 1).join('/');
+                    return (
+                      <Fragment key={subpath}>
+                        /<Link to={`/browse/${subpath}`}>{it}</Link>
+                      </Fragment>
+                    );
+                  })
+                : null}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {path !== '' && (
+            <tr>
+              <th css={FolderTH} scope="row" colSpan={2}>
+                <Link to={path.substr(0, path.lastIndexOf('/')) || '/browse'}>&hellip;</Link>
+              </th>
+            </tr>
+          )}
+          <Suspense maxDuration={500} fallback={<Spinner />}>
+            <ContentList path={path} />
+          </Suspense>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function ContentList({ path }: { path: string }) {
+  return <Contents path={path} contents={BrowserContentsResource.read(cache, path)} />;
 }
 
 type ContentsProps = {
   path: string,
-  contents: FolderData | null,
+  contents: FolderData,
 };
 
 function Contents({ path, contents }: ContentsProps) {
-  if (contents === null) {
-    return (
-      <tr>
-        <td>
-          <CircularProgress size={24} thickness={8} />
-        </td>
-      </tr>
-    );
-  }
-
   const { folders, files } = contents;
   const basePath = path.length ? path + '/' : '';
 
   return (
-    <React.Fragment>
+    <Fragment>
       {folders.map(folder => {
         const fullPath = `${basePath}${folder}`;
         return <Folder key={fullPath} path={fullPath} />;
@@ -57,113 +113,16 @@ function Contents({ path, contents }: ContentsProps) {
       {files.map(file => (
         <File key={`${basePath}${file}`} path={`${basePath}${file}`} />
       ))}
-    </React.Fragment>
+    </Fragment>
   );
 }
 
-const contentCache: ContentCache = {
-  '': {
-    path: '',
-    files: [],
-    folders: ['release', 'stable', 'nightly'],
-  },
-};
-
-type Props = {
-  path: string,
-};
-
-type State = {
-  contents: FolderData | null,
-};
-
-export class Browser extends React.Component<Props, State> {
-  state = {
-    contents: contentCache[this.props.path] || null,
-  };
-
-  mounted: boolean = false;
-
-  async loadData(path: string) {
-    const contents = await fetchContents(path + '/');
-    contents.path = path;
-    if (contents.files === undefined) {
-      contents.files = [];
-    }
-    contents.folders =
-      contents.folders === undefined ? [] : contents.folders.map(name => name.substring(0, name.length - 1));
-
-    // Store in cache
-    contentCache[path] = contents;
-
-    // Make sure we haven't unmounted or changed path while waiting for the resposne
-    if (this.mounted && path === this.props.path) {
-      this.setState({ contents });
-    }
-  }
-
-  static getDerivedStateFromProps(nextProps: Props, prevState: State) {
-    return {
-      contents: contentCache[nextProps.path] || null,
-    };
-  }
-
-  loadDataIfNeeded() {
-    if (this.state.contents === null) {
-      this.loadData(this.props.path);
-    }
-  }
-
-  componentDidUpdate() {
-    this.loadDataIfNeeded();
-  }
-
-  componentDidMount() {
-    this.mounted = true;
-    this.loadDataIfNeeded();
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  render() {
-    const { path } = this.props;
-    const hasParent = path !== '';
-
-    return (
-      <div className="table-responsive-md mt-sm-4">
-        <table className="table mb-0">
-          <thead className="thead-light">
-            <tr>
-              <th colSpan={2}>
-                <IconCloud /> &nbsp;
-                <Link to={'/browse'}>lwjgl</Link>
-                {path.length
-                  ? path.split('/').map((it, i, arr) => {
-                      const subpath = arr.slice(0, i + 1).join('/');
-                      return (
-                        <React.Fragment key={subpath}>
-                          /<Link to={`/browse/${subpath}`}>{it}</Link>
-                        </React.Fragment>
-                      );
-                    })
-                  : null}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {hasParent && (
-              <tr>
-                <th css={FolderTH} scope="row" colSpan={2}>
-                  <Link to={path.substr(0, path.lastIndexOf('/')) || '/browse'}>&hellip;</Link>
-                </th>
-              </tr>
-            )}
-            <Contents path={path} contents={this.state.contents} />
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+function Spinner() {
+  return (
+    <tr>
+      <td>
+        <CircularProgress size={24} thickness={8} />
+      </td>
+    </tr>
+  );
 }
