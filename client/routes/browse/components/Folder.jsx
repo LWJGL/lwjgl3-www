4f7bb12522e150579c5097acb7ce0c661bd15480
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-20 14:04:56 +0300

    refactor: <Folder />

--- client/routes/browse/components/Folder.jsx
+++ client/routes/browse/components/Folder.jsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import { jsx } from '@emotion/core';
 import css from '@emotion/css';
 import { Link } from '@reach/router';
-
 import IconFolder from '~/components/icons/md/Folder';
 
 export const FolderTH = css`
@@ -23,19 +22,20 @@ export const FolderTH = css`
   }
 `;
 
-type Props = {|
+type Props = {
   path: string,
-|};
+};
 
 export class Folder extends React.PureComponent<Props> {
   render() {
-    const parts = this.props.path.split('/');
+    const { path } = this.props;
+    const parts = path.split('/');
     const name = parts[parts.length - 1];
 
     return (
       <tr>
         <th css={FolderTH} colSpan={2}>
-          <Link to={this.props.path}>
+          <Link to={path}>
             <IconFolder /> {name}
           </Link>
         </th>
