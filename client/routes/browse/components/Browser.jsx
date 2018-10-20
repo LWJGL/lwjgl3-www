6ea8eb6d2325e820fff778f7ee0e555225203df3
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-20 14:05:55 +0300

    experimental: use scheduleCallback to show spinner before Suspense

--- client/routes/browse/components/Browser.jsx
+++ client/routes/browse/components/Browser.jsx
@@ -2,17 +2,19 @@
 // @jsx jsx
 import * as React from 'react';
 //$FlowFixMe
-import { unstable_Suspense as Suspense, Fragment } from 'react';
+import { unstable_Suspense as Suspense, Fragment, Component } from 'react';
 import { createCache, createResource } from 'react-cache';
+import { unstable_scheduleCallback } from 'scheduler';
+import { delay } from '~/services/delay';
 
 import { jsx } from '@emotion/core';
 import { Folder, FolderTH } from './Folder';
 import { File } from './File';
-import IconCloud from '~/components/icons/md/Cloud';
-import IconChevronRight from '~/components/icons/md/ChevronRight';
 import { Link } from '@reach/router';
 import { CircularProgress } from '~/components/CircularProgress';
 import { HTTP_OK } from '~/services/http_status_codes';
+import IconCloud from '~/components/icons/md/Cloud';
+import IconChevronRight from '~/components/icons/md/ChevronRight';
 
 type Props = {
   path: string,
@@ -24,7 +26,12 @@ type FolderData = {|
   folders: Array<string>,
 |};
 
-const cache = createCache();
+let cache;
+function invalidateCache() {
+  cache = createCache(invalidateCache);
+}
+invalidateCache();
+
 const BrowserContentsResource = createResource(fetchContents);
 
 async function fetchContents(path: string): Promise<FolderData> {
@@ -49,61 +56,79 @@ async function fetchContents(path: string): Promise<FolderData> {
   contents.folders =
     contents.folders === undefined ? [] : contents.folders.map(name => name.substring(0, name.length - 1));
 
-  return contents;
+  await delay(3000);
+
+  return Promise.resolve(contents);
 }
 
-export function Browser({ path }: Props) {
-  return (
-    <div className="table-responsive-md mt-sm-4">
-      <table className="table mb-0">
-        <thead className="thead-light">
-          <tr>
-            <th colSpan={2}>
-              <IconCloud /> &nbsp;
-              <Link to={'/browse'}>lwjgl</Link>
-              {path.length
-                ? path.split('/').map((it, i, arr) => {
-                    const subpath = arr.slice(0, i + 1).join('/');
-                    return (
-                      <Fragment key={subpath}>
-                        /<Link to={`/browse/${subpath}`}>{it}</Link>
-                      </Fragment>
-                    );
-                  })
-                : null}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {path !== '' && (
-            <tr>
-              <th css={FolderTH} scope="row" colSpan={2}>
-                <Link to={path.substr(0, path.lastIndexOf('/')) || '/browse'}>&hellip;</Link>
-              </th>
-            </tr>
-          )}
-          <Suspense maxDuration={500} fallback={<Spinner />}>
-            <ContentList path={path} />
-          </Suspense>
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
-function ContentList({ path }: { path: string }) {
-  return <Contents path={path} contents={BrowserContentsResource.read(cache, path)} />;
-}
-
-type ContentsProps = {
+type State = {
+  loading: string,
   path: string,
-  contents: FolderData,
 };
 
-function Contents({ path, contents }: ContentsProps) {
-  const { folders, files } = contents;
-  const basePath = path.length ? path + '/' : '';
+export class Browser extends Component<Props, State> {
+  state = {
+    loading: '',
+    path: '',
+  };
 
+  componentDidUpdate() {
+    const { path } = this.props;
+
+    if (this.state.loading !== path) {
+      this.setState({
+        loading: path,
+      });
+      unstable_scheduleCallback(() => this.setState({ path }));
+    }
+  }
+
+  render() {
+    const { path, loading } = this.state;
+
+    return (
+      <div className="table-responsive-md mt-sm-4">
+        <table className="table mb-0">
+          <thead className="thead-light">
+            <tr>
+              <th colSpan={2}>
+                <IconCloud /> &nbsp;
+                <Link to={'/browse'}>lwjgl</Link>
+                {path.length
+                  ? path.split('/').map((it, i, arr) => {
+                      const subpath = arr.slice(0, i + 1).join('/');
+                      return (
+                        <Fragment key={subpath}>
+                          /<Link to={`/browse/${subpath}`}>{it}</Link>
+                        </Fragment>
+                      );
+                    })
+                  : null}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {path !== '' && (
+              <tr>
+                <th css={FolderTH} scope="row" colSpan={2}>
+                  <Link to={path.substr(0, path.lastIndexOf('/')) || '/browse'}>&hellip;</Link>
+                </th>
+              </tr>
+            )}
+            {path !== loading && <Spinner />}
+            <Suspense maxDuration={2000} fallback={<Spinner />}>
+              <Contents path={path} />
+            </Suspense>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+function Contents({ path }: Props) {
+  const { folders, files } = BrowserContentsResource.read(cache, path);
+  const basePath = path.length ? path + '/' : '';
   return (
     <Fragment>
       {folders.map(folder => {
