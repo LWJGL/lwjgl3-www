37eaf12afce11504bc607d534212a39bb1f3d97c
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-27 19:54:32 +0300

    refactor: memo Alert

--- client/components/Alert.jsx
+++ client/components/Alert.jsx
@@ -1,5 +1,7 @@
 // @flow
 import * as React from 'react';
+//$FlowFixMe
+import { memo } from 'react';
 import { Dialog } from './Dialog';
 import { Trap } from './Trap';
 import IconClose from './icons/md/Close';
@@ -15,35 +17,26 @@ type AlertProps = {
   onClose?: () => mixed,
 };
 
-export class Alert extends React.PureComponent<AlertProps, void> {
-  static defaultProps = {
-    type: 'primary',
-    isOpen: true,
-  };
-
-  render() {
-    const { type, isOpen, onClose, title, message } = this.props;
-
-    return (
-      <Dialog className="dialog-alert" isOpen={isOpen} portal={true}>
-        <Trap className="dialog-content" role="alertdialog" onClose={onClose}>
-          {title != null &&
-            title.length > 0 && (
-              <div className={`dialog-header bg-${type}`}>
-                <h3>{title}</h3>
-                <div className="dialog-close-button">
-                  <IconClose onClick={onClose} />
-                </div>
+function Alert({ type = 'primary', isOpen = true, onClose, title, message }: AlertProps) {
+  return (
+    <Dialog className="dialog-alert" isOpen={isOpen} portal={true}>
+      <Trap className="dialog-content" role="alertdialog" onClose={onClose}>
+        {title != null &&
+          title.length > 0 && (
+            <div className={`dialog-header bg-${type}`}>
+              <h3>{title}</h3>
+              <div className="dialog-close-button">
+                <IconClose onClick={onClose} />
               </div>
-            )}
-          <p>{message}</p>
-          <div className="dialog-footer">
-            <button type="button" className={`btn btn-sm btn-default`} onClick={onClose} autoFocus={true}>
-              Close
-            </button>
-          </div>
-        </Trap>
-      </Dialog>
-    );
-  }
+            </div>
+          )}
+        <p>{message}</p>
+        <div className="dialog-footer">
+          <button type="button" className={`btn btn-sm btn-default`} onClick={onClose} autoFocus={true}>
+            Close
+          </button>
+        </div>
+      </Trap>
+    </Dialog>
+  );
 }
