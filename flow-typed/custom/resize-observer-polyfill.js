425208380fc2210443a78657dc64c1ffab136327
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-29 16:05:48 +0200

    feat: deprecate <ResizeObservable> in favor of a Hook: useResizeObserver

--- flow-typed/custom/resize-observer-polyfill.js
+++ flow-typed/custom/resize-observer-polyfill.js
@@ -1,17 +1,3 @@
 declare module 'resize-observer-polyfill' {
-  declare export type Entry = {
-    +target: Element,
-    +contentRect: DOMRectReadOnly,
-  };
-
-  declare export type ResizeObserverCallback = (entries: $ReadOnlyArray<Entry>, observer: ResizeObserver) => void;
-
-  declare class ResizeObserver {
-    constructor(entries: ResizeObserverCallback): void;
-    disconnect(): void;
-    observe(target: Element): void;
-    unobserve(target: Element): void;
-  }
-
   declare export default Class<ResizeObserver>;
 }
