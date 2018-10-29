be03e273d2c46b202d13220b36bc5f41705c9ffc
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-29 17:13:27 +0200

    refactor: use hooks for <Markdown />

--- client/components/Markdown.jsx
+++ client/components/Markdown.jsx
@@ -1,33 +1,18 @@
 // @flow
 import * as React from 'react';
+//$FlowFixMe
+import { memo, useRef, useMutationEffect } from 'react';
 // https://github.com/jonschlinkert/remarkable
 import Remarkable from 'remarkable';
 import type { RemarkableOptions, RemarkableSettings } from 'remarkable';
 
 type Props = {
-  tag: string,
-  source?: string,
-  children?: React.Node,
+  tag?: string,
+  source: string,
   options?: RemarkableOptions | RemarkableSettings,
 };
 
-export const Markdown = ({ tag = 'div', source, children, options, ...rest }: Props) => {
+export const Markdown = memo(({ tag = 'div', source, options, ...rest }: Props) => {
   const Container = tag;
-  const md = new Remarkable(options);
-
-  if (source != null) {
-    return <Container {...rest} dangerouslySetInnerHTML={{ __html: md.render(source) }} />;
-  }
-
-  return (
-    <Container {...rest}>
-      {React.Children.map(children, child => {
-        if (typeof child === 'string') {
-          return <span dangerouslySetInnerHTML={{ __html: md.render(child) }} />;
-        } else {
-          return child;
-        }
-      })}
-    </Container>
-  );
-};
+  return <Container {...rest} dangerouslySetInnerHTML={{ __html: new Remarkable(options).render(source) }} />;
+});
