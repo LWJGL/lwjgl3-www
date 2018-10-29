acb81db534a2b427ed3e18a8b5aa8027ea69d120
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-29 16:05:48 +0200

    feat: deprecate <ResizeObservable> in favor of a Hook: useResizeObserver

---
+++ client/hooks/useResizeObserver.js
@@ -0,0 +1,30 @@
+// @flow
+//$FlowFixMe
+import { useRef, useState, useLayoutEffect } from 'react';
+// import ResizeObserver from 'resize-observer-polyfill';
+
+type HtmlRef = { current: HTMLElement };
+type ObserverRef = { current: null | ResizeObserver };
+
+export function useResizeObserver(target: HtmlRef) {
+  const [rect, setRect] = useState({ width: 0, height: 0, x: 0, y: 0, top: 0, right: 0, bottom: 0, left: 0 });
+  const observer: ObserverRef = useRef(null);
+
+  useLayoutEffect(() => {
+    function handleResize(entries: ResizeObserverEntry[]) {
+      setRect(entries[0].contentRect);
+    }
+
+    observer.current = new ResizeObserver(handleResize);
+    observer.current.observe(target.current);
+
+    return () => {
+      if (observer.current !== null) {
+        observer.current.disconnect();
+        observer.current = null;
+      }
+    };
+  }, []);
+
+  return rect;
+}
