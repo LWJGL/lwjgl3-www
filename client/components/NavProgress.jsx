f4ba0a3f81588ac78d6ff1b38d16410ed220499a
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-10-30 13:09:29 +0200

    refactor: use hooks & context for NavProgress

--- client/components/NavProgress.jsx
+++ client/components/NavProgress.jsx
@@ -1,12 +1,10 @@
 // @flow
 // @jsx jsx
 import * as React from 'react';
+//$FlowFixMe
+import { useState, useEffect, useContext, useRef } from 'react';
 import { jsx, css, keyframes } from '@emotion/core';
 import { COLOR_CUSTOM_CONTROL_INDICATOR_BG, COLOR_CUSTOM_CONTROL_INDICATOR_CHECKED_BG, ZINDEX_FIXED } from '~/theme';
-
-let instance: null | NavProgress = null;
-let count = 0;
-const PERC_MAX = 99.4;
 
 const pulseAnimation = keyframes(css`
   from {
@@ -23,155 +21,153 @@ const pulseAnimation = keyframes(css`
   }
 `);
 
-export function start(delay: number = 0) {
-  count += 1;
-  if (delay > 0) {
-    setTimeout(startInstance, delay);
-  } else {
-    startInstance();
+const cssProgressContainer = css`
+  position: fixed;
+  width: 100%;
+  height: 3px;
+  z-index: ${ZINDEX_FIXED};
+  background: ${COLOR_CUSTOM_CONTROL_INDICATOR_BG.css()};
+  pointer-events: none;
+  will-change: opacity;
+  transition: opacity 0.75s 0.25s ease-out;
+  &.fade-out {
+    opacity: 0;
   }
-}
+`;
 
-function startInstance() {
-  if (count > 0 && instance !== null && instance.mounted) {
-    instance.start();
-  }
-}
+const cssProgressBar = css`
+  width: 100%;
+  height: 3px;
+  background: ${COLOR_CUSTOM_CONTROL_INDICATOR_CHECKED_BG.css()};
+  will-change: transform;
+  transform-origin: left;
+  transition: transform 0.2s linear;
+`;
 
-export function end() {
-  if (count > 0) {
-    count -= 1;
-    if (count === 0 && instance !== null && instance.mounted) {
-      instance.end();
-    }
-  }
-}
+const cssProgressPulse = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 20vw;
+  height: 3px;
+  will-change: transform;
+  transform-origin: left;
+  background: linear-gradient(to right, transparent, ${COLOR_CUSTOM_CONTROL_INDICATOR_BG.css()} 50%, transparent);
+  animation: ${pulseAnimation.name} 2.5s linear 10s both infinite;
+  ${pulseAnimation.styles};
+`;
 
-type State = {
-  progress: number,
+type Context = {
+  start: (delay: number) => void,
+  end: () => void,
 };
 
-export class NavProgress extends React.Component<{||}, State> {
-  mounted = false;
-  trickleTimeoutId: TimeoutID | null = null;
+type ProviderProps = {
+  children: React.Node,
+};
 
-  state = {
-    progress: 0,
+export const NavProgressContext = React.createContext<Context>({
+  start: () => {},
+  end: () => {},
+});
+
+export function NavProgressProvider({ children }: ProviderProps) {
+  const [count, setCount] = useState(0);
+  const delayTimeout = useRef(null);
+
+  function start(delay: number = 0) {
+    if (delay > 0) {
+      delayTimeout.current = setTimeout(() => setCount(count => count + 1), delay);
+    } else {
+      setCount(count + 1);
+    }
+  }
+
+  function end() {
+    if (count <= 0 && !FLAG_PRODUCTION) {
+      throw new Error('Called end without first calling start');
+    }
+    if (delayTimeout.current !== null) {
+      clearTimeout(delayTimeout.current);
+      delayTimeout.current = null;
+    } else {
+      setCount(count - 1);
+    }
+  }
+
+  const state = {
+    count,
+    start,
+    end,
   };
 
-  componentDidMount() {
-    this.mounted = true;
-    if (instance === null) {
-      instance = this;
-    }
-  }
+  return <NavProgressContext.Provider value={state}>{React.Children.only(children)}</NavProgressContext.Provider>;
+}
 
-  componentWillUnmount() {
-    this.mounted = false;
-    if (instance === this) {
-      instance = null;
-    }
-  }
+const PERC_MAX = 99.4;
 
-  start() {
-    this.setState({ progress: 1 });
-    this.trickleTimeoutId = setTimeout(this.trickle.bind(this), 0);
-  }
+export function NavProgress() {
+  const { count } = useContext(NavProgressContext);
+  const [progress, setProgress] = useState(0);
+  const trickleTimeoutId = useRef(null);
 
-  trickle() {
-    let { progress } = this.state;
-    if (this.mounted && progress < PERC_MAX) {
-      let step = 0;
+  useEffect(
+    () => {
+      function trickle() {
+        setProgress(progress => {
+          if (progress < PERC_MAX) {
+            let step = 0;
 
-      if (progress < 37) {
-        step = 13;
-      } else if (progress >= 37 && progress < 59) {
-        step = 7;
-      } else if (progress >= 59 && progress < 83) {
-        step = 3;
-      } else {
-        step = 0.5;
-      }
+            if (progress < 37) {
+              step = 13;
+            } else if (progress >= 37 && progress < 59) {
+              step = 7;
+            } else if (progress >= 59 && progress < 83) {
+              step = 3;
+            } else {
+              step = 0.5;
+            }
 
-      progress += step;
-      if (progress > PERC_MAX) {
-        progress = PERC_MAX;
-      }
-
-      this.setState({ progress: progress });
-      this.trickleTimeoutId = setTimeout(this.trickle.bind(this), 200 + Math.floor(400 * Math.random()));
-    }
-  }
-
-  end() {
-    if (this.state.progress > 0) {
-      this.setState({ progress: 100 });
-      setTimeout(this.reset.bind(this), 400);
-    }
-    if (this.trickleTimeoutId !== null) {
-      clearTimeout(this.trickleTimeoutId);
-      this.trickleTimeoutId = null;
-    }
-  }
-
-  reset() {
-    if (count === 0) {
-      this.setState({ progress: 0 });
-    }
-  }
-
-  render() {
-    const { progress } = this.state;
-    return progress === 0 ? null : (
-      <div
-        className={progress === 100 ? 'fade-out' : ''}
-        css={css`
-          position: fixed;
-          width: 100%;
-          height: 3px;
-          z-index: ${ZINDEX_FIXED};
-          background: ${COLOR_CUSTOM_CONTROL_INDICATOR_BG.css()};
-          pointer-events: none;
-          will-change: opacity;
-          transition: opacity 0.3s ease-out;
-          &.fade-out {
-            opacity: 0;
+            trickleTimeoutId.current = setTimeout(trickle, 200 + Math.floor(400 * Math.random()));
+            return progress >= PERC_MAX ? PERC_MAX : progress + step;
           }
-        `}
-      >
-        <div
-          style={{
-            transform: `scaleX(${progress / 100})`,
-          }}
-          css={css`
-            width: 100%;
-            height: 3px;
-            background: ${COLOR_CUSTOM_CONTROL_INDICATOR_CHECKED_BG.css()};
-            will-change: transform;
-            transform-origin: left;
-            transition: transform 0.2s linear;
-          `}
-        />
-        <div
-          css={css`
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 20vw;
-            height: 3px;
-            will-change: transform;
-            transform-origin: left;
-            background: linear-gradient(
-              to right,
-              transparent,
-              ${COLOR_CUSTOM_CONTROL_INDICATOR_BG.css()} 50%,
-              transparent
-            );
-            animation: ${pulseAnimation.name} 2.5s linear 10s both infinite;
-            ${pulseAnimation.styles};
-          `}
-        />
-      </div>
-    );
-  }
+        });
+      }
+
+      function reset() {
+        if (count === 0) {
+          setProgress(0);
+        }
+      }
+
+      if (count > 0) {
+        if (progress === 0) {
+          trickle();
+        }
+      } else if (progress > 0) {
+        // Fill bar and wait 400ms before resetting
+        setProgress(100);
+        setTimeout(reset, 750);
+
+        // Clear tricle timeout
+        if (trickleTimeoutId.current !== null) {
+          clearTimeout(trickleTimeoutId.current);
+          trickleTimeoutId.current = null;
+        }
+      }
+    },
+    [count]
+  );
+
+  return (
+    <div hidden={progress === 0} className={progress === 100 ? 'fade-out' : ''} css={cssProgressContainer}>
+      <div
+        css={cssProgressBar}
+        style={{
+          transform: `scaleX(${progress / 100})`,
+        }}
+      />
+      <div css={cssProgressPulse} />
+    </div>
+  );
 }
--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Layout } from './Layout';
 import { BreakpointProvider } from '../components/Breakpoint';
 import { ServiceWorkerProvider } from '../components/ServiceWorker';
+import { NavProgressProvider } from '../components/NavProgress';
 
 // Pull common modules on main bundle
 import '../components/routes/PageView';
@@ -12,7 +13,9 @@ export default function App() {
   return (
     <ServiceWorkerProvider>
       <BreakpointProvider>
-        <Layout />
+        <NavProgressProvider>
+          <Layout />
+        </NavProgressProvider>
       </BreakpointProvider>
     </ServiceWorkerProvider>
   );
