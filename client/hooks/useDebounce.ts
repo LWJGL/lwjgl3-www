defc2264a7a911061954b297c02c535c2ba15c7a
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2018-11-11 19:38:41 +0200

    refactor: simplify useDebounce

--- client/hooks/useDebounce.ts
+++ client/hooks/useDebounce.ts
@@ -1,18 +1,15 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 export function useDebounce<T>(value: T, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value);
-  const handler: React.MutableRefObject<number | undefined> = useRef(undefined);
 
   useEffect(
     () => {
-      handler.current = window.setTimeout(() => {
+      const handler = window.setTimeout(() => {
         setDebouncedValue(value);
       }, delay);
 
-      return () => {
-        window.clearTimeout(handler.current);
-      };
+      return () => window.clearTimeout(handler);
     },
     [value, delay]
   );
