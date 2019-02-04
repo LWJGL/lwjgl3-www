d2760d4f276bb1628246b9561d58edc244f90d37
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2019-02-04 13:24:43 +0200

    feat: add useTimeout & useInterval hooks

---
+++ client/hooks/useInterval.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+export function useInterval(callback: Function, delay: number | null) {
+  const savedCallback: React.MutableRefObject<Function> = useRef(callback);
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
