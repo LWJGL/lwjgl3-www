0e3781e3146f407568254f12e82723db0cf8b6d5
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2019-09-07 03:30:53 +0300

    build: exit on error with code > 0

--- scripts/build-production.js
+++ scripts/build-production.js
@@ -8,56 +8,56 @@ config.plugins.push(new webpack.ProgressPlugin());
 const compiler = webpack(config);
 
 compiler.run((err, stats) => {
-  if (err) {
-    console.error(err.stack || err);
-    if (err.details) {
-      console.error(err.details);
-    }
-    return;
-  }
-
-  if (stats.hasErrors()) {
-    console.error(
-      stats.toString({
-        colors: true,
-        all: false,
-        maxModules: 0,
-        errors: true,
-        errorDetails: true,
-        warnings: false,
-      })
-    );
-    return;
-  }
-
-  if (stats.hasWarnings()) {
-    console.error(
-      stats.toString({
-        colors: true,
-        all: false,
-        maxModules: 0,
-        warnings: true,
-      })
-    );
-  }
-
-  fs.writeFileSync(
-    path.resolve(__dirname, '../public/js/webpack.manifest.json'),
-    JSON.stringify(
-      stats.toJson({
-        colors: false,
-        // all: false,
-        all: true,
-        entrypoints: true,
-        assets: true,
-        chunkGroups: true,
-      }),
-      null,
-      2
-    )
-  );
-
   compiler.close(() => {
-    // all done
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      process.exit(1);
+      return;
+    }
+
+    if (stats.hasErrors()) {
+      console.error(
+        stats.toString({
+          colors: true,
+          all: false,
+          maxModules: 0,
+          errors: true,
+          errorDetails: true,
+          warnings: false,
+        })
+      );
+      process.exit(1);
+      return;
+    }
+
+    if (stats.hasWarnings()) {
+      console.error(
+        stats.toString({
+          colors: true,
+          all: false,
+          maxModules: 0,
+          warnings: true,
+        })
+      );
+    }
+
+    fs.writeFileSync(
+      path.resolve(__dirname, '../public/js/webpack.manifest.json'),
+      JSON.stringify(
+        stats.toJson({
+          colors: false,
+          // all: false,
+          all: true,
+          entrypoints: true,
+          assets: true,
+          chunkGroups: true,
+        }),
+        null,
+        2
+      )
+    );
   });
 });
