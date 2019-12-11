048501dc21012b43bf83107b48b0cfdf8014b617
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2019-12-11 18:45:56 +0200

    ui: improve error page design

--- client/components/routes/PageError.tsx
+++ client/components/routes/PageError.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useDocumentTitle } from '~/hooks/useDocumentTitle';
 import { ErrorProps } from '../ErrorBoundary';
+import { COLOR_RED } from '~/theme';
 
 export function PageError({ error }: ErrorProps) {
   useDocumentTitle('An error has occured');
 
   return (
-    <main>
-      <section className="container text-center py-5">
+    <main style={{ backgroundColor: COLOR_RED.css(), color: '#fff' }}>
+      <section className="container text-center pt-5 pb-3">
         <h1>Something went wrong</h1>
-        <h3 className="text-danger">Oh no! It appears that the page has crashed or failed to load.</h3>
+        <h3>Oh no! It appears that the page has crashed or failed to load.</h3>
       </section>
       <hr />
-      <section className="container small py-5 text-muted">
+      <section className="container pt-3 pb-5 text-light" style={{ marginBottom: '-1rem' }}>
         <h5>Error information</h5>
         <p>{error.message}</p>
+        <pre className="text-warning">{error.stack}</pre>
       </section>
     </main>
   );
