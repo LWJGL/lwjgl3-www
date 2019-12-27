b1b5504c6a0b2aaf6f59f57d6294cef41aff33fe
Ioannis Tsakpinis <iotsakp@gmail.com>
2019-12-27 18:59:42 +0200

    feat: add driftfx & meshoptimizer bindings

--- client/routes/customize/versions/nightly.ts
+++ client/routes/customize/versions/nightly.ts
@@ -14,6 +14,22 @@ export default (prev: BuildBindings): BuildBindings => ({
       natives: NATIVE_ALL,
       website: 'http://bulletphysics.org/',
     },
+    [Binding.DRIFTFX]: {
+      id: Binding.DRIFTFX,
+      title: 'DriftFX',
+      description:
+          'A library that allows you to render any OpenGL content directly into JavaFX nodes.',
+      natives: NATIVE_ALL,
+      website: 'http://bulletphysics.org/',
+    },
+    [Binding.MESHOPTIMIZER]: {
+      id: Binding.MESHOPTIMIZER,
+      title: 'meshoptimizer',
+      description:
+          'A mesh optimization library that makes meshes smaller and faster to render.',
+      natives: NATIVE_ALL,
+      website: 'http://bulletphysics.org/',
+    },
     [Binding.SPVC]: {
       id: Binding.SPVC,
       title: 'SPIRV-Cross',
