2fd39288e09fa11277db44d6c449ab1060a42165
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2020-04-05 23:56:11 +0300

    style: upgrade & reformat with prettier

--- client/components/icons/fa/duotone/copy.ts
+++ client/components/icons/fa/duotone/copy.ts
@@ -1,2 +1,4 @@
 import { register } from '~/components/icons/sheet';
-register(`<symbol id="fa/duotone/copy" viewBox="0 0 448 512"><path d="M352 96V0H152a24 24 0 0 0-24 24v368a24 24 0 0 0 24 24h272a24 24 0 0 0 24-24V96z" class="s"/><path d="M96 392V96H24a24 24 0 0 0-24 24v368a24 24 0 0 0 24 24h272a24 24 0 0 0 24-24v-40H152a56.06 56.06 0 0 1-56-56zM441 73L375 7a24 24 0 0 0-17-7h-6v96h96v-6.06A24 24 0 0 0 441 73z"/></symbol>`);
+register(
+  `<symbol id="fa/duotone/copy" viewBox="0 0 448 512"><path d="M352 96V0H152a24 24 0 0 0-24 24v368a24 24 0 0 0 24 24h272a24 24 0 0 0 24-24V96z" class="s"/><path d="M96 392V96H24a24 24 0 0 0-24 24v368a24 24 0 0 0 24 24h272a24 24 0 0 0 24-24v-40H152a56.06 56.06 0 0 1-56-56zM441 73L375 7a24 24 0 0 0-17-7h-6v96h96v-6.06A24 24 0 0 0 441 73z"/></symbol>`
+);
