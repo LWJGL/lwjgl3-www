4b17a3ab1d3f1cb7f09813273696e5a8591bfc01
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2020-05-16 05:22:49 +0300

    feat: add createLazyEmitter

---
+++ client/services/createLazyEmitter.ts
@@ -0,0 +1,62 @@
+type Init<Args extends any[]> = (emit: Emit<Args>) => Cleanup;
+type Emit<Args extends any[]> = (...args: Args) => void;
+type Cleanup = () => void;
+type Subscriber<Args extends any[]> = (...args: Args) => void;
+
+/*
+  USAGE:
+
+  let windowClicks = createLazyEmitter((emit) => {
+    console.log('started');
+
+    function onClick() {
+      emit('arg1', 'arg2');
+    }
+
+    window.addEventListener('click', onClick);
+    return () => {
+      window.removeEventListener('click', onClick);
+      console.log('stopped');
+    };
+  });
+
+  let unsubscribe1 = windowClicks((arg1, arg2) => {
+    ...
+  });
+  // logs: "started"
+  let unsubscribe2 = windowClicks((arg1, arg2) => {
+    ...
+  });
+
+  unsubscribe1();
+  unsubscribe2();
+  // logs: "stopped"
+*/
+
+export function createLazyEmitter<Args extends any[]>(init: Init<Args>) {
+  const subscribers = new Set<Subscriber<Args>>();
+  let cleanup: Cleanup | 0 = 0;
+
+  const emit = (...args: Args) => {
+    subscribers.forEach((subscriber) => subscriber(...args));
+  };
+
+  const subscribe = (subscriber: Subscriber<Args>) => {
+    subscribers.add(subscriber);
+
+    if (cleanup === 0) {
+      cleanup = init(emit);
+    }
+
+    return () => {
+      subscribers.delete(subscriber);
+
+      if (subscribers.size === 0) {
+        (cleanup as Cleanup)();
+        cleanup = 0;
+      }
+    };
+  };
+
+  return subscribe;
+}
