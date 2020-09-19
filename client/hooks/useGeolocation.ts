1b16116cb593220c36d6b2a9c554142d91917a1d
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2020-09-19 16:58:25 +0300

    feat: simplify useGeo state, fix TS error

--- client/hooks/useGeolocation.ts
+++ client/hooks/useGeolocation.ts
@@ -1,16 +1,15 @@
 import { useState, useEffect } from 'react';
 
 export function useGeolocation(options: PositionOptions) {
-  const [position, setPosition] = useState({
-    timestamp: Date.now(),
-    coords: {},
-  });
-  const [error, setError] = useState<PositionError | null>(null);
+  const [position, setPosition] = useState<GeolocationPosition | null>(null);
+  const [error, setError] = useState<GeolocationPositionError | null>(null);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(setPosition, setError, options);
     const watchId = navigator.geolocation.watchPosition(setPosition, setError, options);
-    return () => navigator.geolocation.clearWatch(watchId);
+    return () => {
+      navigator.geolocation.clearWatch(watchId);
+    };
   }, [options]);
 
   return { position, error };
