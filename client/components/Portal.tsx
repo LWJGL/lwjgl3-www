2c41b8b541879147277768d820c5d3b597748444
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2020-10-17 05:00:03 +0300

    refactor: do not create unnecessary elements for Portal

--- client/components/Portal.tsx
+++ client/components/Portal.tsx
@@ -1,25 +1,5 @@
-import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-/**
- * This component detaches its contents and re-attaches them to document.body (using ReactDOM.createPortal).
- * Use it when you need to circumvent DOM z-stacking (for dialogs, popovers, etc.).
- */
 export const Portal: React.FC = ({ children }) => {
-  const targetElement = useRef<HTMLDivElement | null>(null);
-
-  if (targetElement.current === null) {
-    targetElement.current = document.body.appendChild(document.createElement('div'));
-  }
-
-  useEffect(() => {
-    return () => {
-      if (targetElement.current !== null) {
-        document.body.removeChild(targetElement.current);
-        targetElement.current = null;
-      }
-    };
-  }, []);
-
-  return createPortal(children !== undefined ? children : null, targetElement.current);
+  return createPortal(children !== undefined ? children : null, document.body);
 };
