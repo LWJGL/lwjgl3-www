ab7ce2e04a44eada3907b2d00957ef68c4aeedb5
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2020-12-28 17:54:56 +0200

    feat: cli to create a Cloudfront invalidation request

---
+++ scripts/flush.mjs
@@ -0,0 +1,36 @@
+import { CloudFront } from '@aws-sdk/client-cloudfront';
+
+const distributionIds = {
+  www: 'ENXBPW89N98ZQ',
+  build: 'E23P50F0J5ZE30',
+  javadoc: 'E2128JB5XYS0E',
+  blog: 'E1S5ZLF6PKR47Q',
+};
+
+let distribution = 'www';
+
+process.argv.slice(2).forEach(arg => {
+  switch (arg) {
+    case 'build':
+    case 'javadoc':
+    case 'blog':
+      distribution = arg;
+      break;
+  }
+});
+
+const cloudfront = new CloudFront({ region: 'us-east-1' });
+
+const output = await cloudfront.createInvalidation({
+  DistributionId: distributionIds[distribution],
+  InvalidationBatch: {
+    CallerReference: Date.now(),
+    Paths: {
+      Quantity: 1,
+      Items: ['/'],
+    },
+  },
+});
+
+console.log(output.Invalidation);
+console.log(output.Location);
