176ade02f269888cb05fac7ae2aa7d4d8fef98a8
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2021-01-01 02:39:18 +0200

    feat: add script to update ECS service
    This requires AWS credentials

---
+++ scripts/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws ecs update-service \
+--cluster "arn:aws:ecs:us-east-1:668135804372:cluster/webhotelier" \
+--service "arn:aws:ecs:us-east-1:668135804372:service/webhotelier/lwjgl-www" \
+--force-new-deployment
