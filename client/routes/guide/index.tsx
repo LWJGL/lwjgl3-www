21bf449953aed30e7ac22a1829d32f3f5bb8dd92
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2021-02-08 16:29:17 +0200

    build: upgrade deps

--- client/routes/guide/index.tsx
+++ client/routes/guide/index.tsx
@@ -11,7 +11,7 @@ import { Title } from '~/components/lwjgl/Title';
 import { Dark } from '~/components/lwjgl/Dark';
 
 const Sample: React.FC<{ children?: never }> = () => {
-  useCSS('https://cdn.jsdelivr.net/npm/highlight.js@10.5.0/styles/darcula.min.css');
+  useCSS('https://cdn.jsdelivr.net/npm/highlight.js@10.6.0/styles/darcula.min.css');
   const sample = readSample();
 
   return (
