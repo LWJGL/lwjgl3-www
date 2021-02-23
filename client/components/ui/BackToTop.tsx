0be885328802c576d09dc49696293422ef0b7c1e
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2021-02-23 22:28:53 +0200

    fix: typo

--- client/components/ui/BackToTop.tsx
+++ client/components/ui/BackToTop.tsx
@@ -3,13 +3,13 @@ import { Button } from '~/components/forms/Button';
 import { Icon } from '~/components/ui/Icon';
 import '~/theme/icons/fa/regular/arrow-to-top';
 
-function scrollTopTop(e: React.MouseEvent<HTMLButtonElement>) {
+function scrollToTop(e: React.MouseEvent<HTMLButtonElement>) {
   e.preventDefault();
   scrollSmooth(0, 0);
 }
 
 export const BackToTop: React.FC<{ children?: never }> = () => (
-  <Button variant="text" tone="neutral" onClick={scrollTopTop}>
+  <Button variant="text" tone="neutral" onClick={scrollToTop}>
     Top <Icon name="fa/regular/arrow-to-top" />
   </Button>
 );
