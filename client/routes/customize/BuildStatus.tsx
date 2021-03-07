4a696e36164b4d3fc782d192bbdaeadbef46d907
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2021-03-08 00:34:07 +0200

    chore: do not render build number for releases, can be confusing

--- client/routes/customize/BuildStatus.tsx
+++ client/routes/customize/BuildStatus.tsx
@@ -15,12 +15,20 @@ const fmt = new Intl.DateTimeFormat([], {
   timeZoneName: 'short',
 });
 
+function renderVersion(version: string, name: BuildType) {
+  if (name !== 'release') {
+    return version;
+  }
+
+  return version.split('build')[0].trim();
+}
+
 export const BuildStatus = ({ name }: Props) => {
   const status = readStatus(name);
 
   return (
     <Text size="sm" css={{ fontFamily: '$mono', color: 'error' in status ? '$critical700' : 'inherit' }}>
-      {'error' in status ? status.error : status.version}
+      {'error' in status ? status.error : renderVersion(status.version, name)}
       <br />
       {'lastModified' in status ? fmt.format(new Date(status.lastModified)) : <br />}
     </Text>
