7cd6e2af91dab4b31f1055eb33af8343681b56c4
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2021-06-09 12:23:48 +0300

    feat: add hook to share forwardedRefs

---
+++ client/hooks/useShareForwardedRef.ts
@@ -0,0 +1,23 @@
+import { useRef, useEffect } from 'react';
+
+export function useShareForwardedRef<T>(forwardedRef: React.Ref<T>) {
+  // final ref that will share value with forward ref. this is the one we will attach to components
+  const innerRef = useRef<T>(null);
+
+  useEffect(() => {
+    // after every render - try to share current ref value with forwarded ref
+    if (!forwardedRef) {
+      return;
+    }
+    if (typeof forwardedRef === 'function') {
+      forwardedRef(innerRef.current);
+      return;
+    } else {
+      // @ts-ignore
+      // by default forwardedRef.current is readonly. Let's ignore it
+      forwardedRef.current = innerRef.current;
+    }
+  });
+
+  return innerRef;
+}
