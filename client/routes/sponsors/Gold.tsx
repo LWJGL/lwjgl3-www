525d56e1d741537d259b3323851f64dd998ccc24
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2021-11-17 02:04:22 +0200

    chore: remove cancelled sponsors

--- client/routes/sponsors/Gold.tsx
+++ client/routes/sponsors/Gold.tsx
@@ -1,6 +1,5 @@
 import { ImgLazy } from '~/components/ui/ImgLazy';
 import { Grid } from '~/components/layout/Grid';
-import { FlexStack } from '~/components/layout/FlexStack';
 import { TitleSection } from '~/components/lwjgl/TitleSection';
 import { Dark } from '~/components/lwjgl/Dark';
 import { SectionContainer } from '~/components/ui/Section';
@@ -33,39 +32,20 @@ export const GoldSponsors: React.FC = ({ children }) => (
         }}
       >
         <a
+          href="https://www.vpsserver.com/"
+          title="VPS HOSTING - GET YOUR FREE VPS TRIAL NOW!"
+          rel="sponsored noopener external"
+          target="_blank"
+        >
+          <ImgLazy width={218} height={50} src="/img/sponsors/vpsserver.com.svg" alt="VPSSERVER.com" />
+        </a>
+        <a
           href="https://www.casinotop.com/"
           title="Online Casino Guide in Canada - Best Gaming Experience!"
           rel="sponsored noopener external"
           target="_blank"
         >
           <ImgLazy width={130 * 1.5} height={20 * 1.5} src="/img/sponsors/casinotop-129x20.svg" alt="CASINOTOP" />
-        </a>
-        {/* <a
-          href="https://www.parhaatnettikasinot.com/"
-          title="Kaikki luotettavat ja parhaat nettikasinot"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy
-            width={330.8 * 0.75}
-            height={46.4 * 0.75}
-            src="/img/sponsors/parhaat-nettikasinot.svg"
-            alt="parhaat nettikasinot"
-          />
-        </a> */}
-        <a
-          href="https://nettikasinot247.fi/"
-          title="KÄRKITASON NETTIKASINOT"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy
-            width={467 * 0.5}
-            height={112 * 0.5}
-            src="/img/sponsors/nettikasinot-logo-467x112.png"
-            alt="NETTIKASINOT247"
-            css={{ filter: 'invert(90%)' }}
-          />
         </a>
         <a
           href="https://nettikasinolista.com/"
@@ -76,80 +56,12 @@ export const GoldSponsors: React.FC = ({ children }) => (
           <ImgLazy width={225} height={60} src="/img/sponsors/nettikasinolista.svg" alt="Nettikasino-lista" />
         </a>
         <a
-          href="https://www.turtlebet.com/fi/kaikki-nettikasinot.html"
-          title="Kaikki nettikasinot - Katso Turtlebetin kaikki kasinot lista!"
+          href="https://casinomartini.com/nz/new-online-casinos/"
+          title="New Zealand New Online Casinos | Full List of New Casino Sites NZ"
           rel="sponsored noopener external"
           target="_blank"
         >
-          <ImgLazy
-            width={173}
-            height={40}
-            src="/img/sponsors/turtlebet-173x40.svg"
-            alt="Turtlebet"
-            css={{ filter: 'invert(100%) hue-rotate(180deg)' }}
-          />
-        </a>
-        <a
-          href="https://www.vpsserver.com/"
-          title="VPS HOSTING - GET YOUR FREE VPS TRIAL NOW!"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={218} height={50} src="/img/sponsors/vpsserver.com.svg" alt="VPSSERVER.com" />
-        </a>
-        <a
-          href="https://buy.fineproxy.org/eng/"
-          rel="sponsored noopener external"
-          target="_blank"
-          title="BUY PROXY FROM FINEPROXY"
-        >
-          <ImgLazy
-            width={250 / 1.25}
-            height={70 / 1.25}
-            src="/img/sponsors/fineproxy-logo-250x70.png"
-            alt="Fineproxy"
-            css={{ filter: 'invert(90%)' }}
-          />
-        </a>
-        <a
-          href="https://www.bonus.net.nz/free-spins-no-deposit"
-          title="Free Spins No Deposit"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={179.71} height={38} src="/img/sponsors/bonusfindernz.svg" alt="BonusFinder New Zealand" />
-        </a>
-        <a
-          href="https://www.kasinot.fi/"
-          title="Kasinot netissä: #1 opas nettikasinoiden maailmaan"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={384 / 3} height={128 / 3} src="/img/sponsors/kasinot-384x128.png" alt="Kasinot.fi" />
-        </a>
-        <a
-          href="http://nettikasinot.org/"
-          rel="sponsored noopener external"
-          target="_blank"
-          title="Universumin parhaat nettikasinot"
-        >
-          <ImgLazy width={585 / 3} height={116 / 3} src="/img/sponsors/nettikasinot-585x116.png" alt="Nettikasinot" />
-        </a>
-        <a
-          href="https://www.pelisivut.com/"
-          title="Rahapelit netissä laadukkaasti ja luotettavasti"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={620 / 3} height={152 / 3} src="/img/sponsors/pelisivut-620x152.png" alt="PELISIVUT" />
-        </a>
-        <a
-          href="https://www.casinotopp.net/"
-          title="Velkommen til CasinoTopp – Norges beste portal til online casino"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={144 * 1.25} height={20 * 1.25} src="/img/sponsors/casinotopp-143x20.svg" alt="CASINOTOPP" />
+          <ImgLazy width={379 / 2} height={138 / 2} src="/img/sponsors/casino-martini.png" alt="Casino Martini" />
         </a>
         <a
           href="https://www.bonusfinder.com/"
@@ -160,12 +72,28 @@ export const GoldSponsors: React.FC = ({ children }) => (
           <ImgLazy width={179.71} height={38} src="/img/sponsors/bonusfinderus.svg" alt="BonusFinder USA" />
         </a>
         <a
-          href="https://kajino.com/"
-          title="オンラインカジノ リストとランキング 2021 - カジノ .com | Kajino"
+          href="https://www.bonusfinder.co.uk/"
+          title="Best Online Gambling Sites UK"
           rel="sponsored noopener external"
           target="_blank"
         >
-          <ImgLazy width={189} height={30} src="/img/sponsors/kajino.com-189x30.svg" alt="Kajino.com" />
+          <ImgLazy width={318 / 1.5} height={64 / 1.5} src="/img/sponsors/bonusfinder-uk.svg" alt="BonusFinder UK" />
+        </a>
+        <a
+          href="https://www.bonus.net.nz/free-spins-no-deposit"
+          title="Free Spins No Deposit"
+          rel="sponsored noopener external"
+          target="_blank"
+        >
+          <ImgLazy width={179.71} height={38} src="/img/sponsors/bonusfindernz.svg" alt="BonusFinder New Zealand" />
+        </a>
+        <a
+          href="http://nettikasinot.org/"
+          rel="sponsored noopener external"
+          target="_blank"
+          title="Universumin parhaat nettikasinot"
+        >
+          <ImgLazy width={585 / 3} height={116 / 3} src="/img/sponsors/nettikasinot-585x116.png" alt="Nettikasinot" />
         </a>
         <a
           href="https://www.nettikasinot.media/"
@@ -176,38 +104,14 @@ export const GoldSponsors: React.FC = ({ children }) => (
           <ImgLazy width={48} height={48} src="/img/sponsors/nettikasinot.media.svg" alt="nettikasinot.media" />
         </a>
         <a
-          href="https://www.connessionivpn.it/"
-          title="Connessioni VPN"
+          href="https://www.pelisivut.com/"
+          title="Rahapelit netissä laadukkaasti ja luotettavasti"
           rel="sponsored noopener external"
           target="_blank"
         >
-          <FlexStack
-            gap="0.5rem"
-            css={{
-              color: '#577BFA',
-              fontWeight: 'bold',
-              '@md': {
-                justifyContent: 'center',
-              },
-            }}
-          >
-            <ImgLazy
-              width={460 / 8}
-              height={279 / 8}
-              src="/img/sponsors/connessionivpn.it-460x279.svg"
-              alt="Connessioni VPN"
-            />{' '}
-            <span>CONNESSIONI VPN</span>
-          </FlexStack>
+          <ImgLazy width={620 / 3} height={152 / 3} src="/img/sponsors/pelisivut-620x152.png" alt="PELISIVUT" />
         </a>
-        <a
-          href="https://casinomartini.com/nz/new-online-casinos/"
-          title="New Zealand New Online Casinos | Full List of New Casino Sites NZ"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={379 / 2} height={138 / 2} src="/img/sponsors/casino-martini.png" alt="Casino Martini" />
-        </a>
+
         <a
           href="https://goread.io/buy-instagram-followers"
           title="Buy Instagram Followers with Instant Delivery"
@@ -215,35 +119,6 @@ export const GoldSponsors: React.FC = ({ children }) => (
           target="_blank"
         >
           <ImgLazy width={1133 * 0.25} height={218 * 0.25} src="/img/sponsors/goread-1133x218.png" alt="Goread.io" />
-        </a>
-        {/* <a
-          href="https://www.boekonomi.se/"
-          title="BoEkonomi.se | Vi hjälper dig ta hand om din ekonomi och ditt boende!"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={200 / 1.5} height={31 / 1.5} src="/img/sponsors/boekonomi-200x31.png" alt="BoEkonomi.se" />
-        </a> */}
-        <a
-          href="https://poprey.com/instagram_views"
-          title="Buy Instagram Views"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={90} height={90} src="/img/sponsors/buy-instagram-views.png" alt="" />
-        </a>
-        <a
-          href="https://anbefaltcasino.com/"
-          title="Beste norske nettcasinoer finner du på AnbefaltCasino.com"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy
-            width={154.219 * 1.2}
-            height={14.594 * 1.2}
-            src="/img/sponsors/sanbefaltcasino.svg"
-            alt="AnbefaltCasino.com"
-          />
         </a>
         <a
           href="https://list.casino/"
@@ -254,14 +129,6 @@ export const GoldSponsors: React.FC = ({ children }) => (
           <ImgLazy width={447 / 2} height={119 / 2} src="/img/sponsors/list-casino.svg" alt="list.casino" />
         </a>
         <a
-          href="https://www.bonusfinder.co.uk/"
-          title="Best Online Gambling Sites UK"
-          rel="sponsored noopener external"
-          target="_blank"
-        >
-          <ImgLazy width={318 / 1.5} height={64 / 1.5} src="/img/sponsors/bonusfinder-uk.svg" alt="BonusFinder UK" />
-        </a>
-        <a
           href="https://casinoenlineahex.com/"
           title="CasinoEnLineaHEX"
           rel="sponsored noopener external"
@@ -270,7 +137,7 @@ export const GoldSponsors: React.FC = ({ children }) => (
           <ImgLazy
             width={2565}
             height={734}
-            style={{ width: 2565 / 13, height: 734 / 13 }}
+            style={{ width: 2565 / 20, height: 734 / 20 }}
             src="/img/sponsors/casinoenlineahex.svg"
             alt="CasinoEnLineaHEX"
           />
@@ -278,11 +145,14 @@ export const GoldSponsors: React.FC = ({ children }) => (
         <a href="https://nzcasinohex.com/" title="NZCasinoHex" rel="sponsored noopener external" target="_blank">
           <ImgLazy width={200 / 2} height={51 / 2} src="/img/sponsors/nzcasinohex.png" alt="NZCasinoHex" />
         </a>
-
         <a href="https://arabcasinohex.com/" title="ArabCasinoHEX" rel="sponsored noopener external" target="_blank">
-          <ImgLazy width={166} height={46} src="/img/sponsors/arab-casino-hex-dark.png" alt="ArabCasinoHEX" />
+          <ImgLazy
+            width={166 / 1.5}
+            height={46 / 1.5}
+            src="/img/sponsors/arab-casino-hex-dark.png"
+            alt="ArabCasinoHEX"
+          />
         </a>
-
         <a
           href="https://www.targetedwebtraffic.com/"
           title="Targeted Web Traffic"
@@ -290,13 +160,12 @@ export const GoldSponsors: React.FC = ({ children }) => (
           target="_blank"
         >
           <ImgLazy
-            width={270 / 3}
-            height={243 / 3}
+            width={270 / 4}
+            height={243 / 4}
             src="/img/sponsors/targetedwebtraffic-270x243.png"
             alt="Targeted Web Traffic"
           />
         </a>
-
         <a
           href="https://www.instafollowers.co/"
           title="Buy Instagram Followers"
@@ -304,8 +173,8 @@ export const GoldSponsors: React.FC = ({ children }) => (
           target="_blank"
         >
           <ImgLazy
-            width={320 / 1.5}
-            height={100 / 1.5}
+            width={320 / 1.75}
+            height={100 / 1.75}
             src="/img/sponsors/instafollowers320x100.svg"
             alt="instafollowers.co"
           />
--- client/routes/sponsors/index.tsx
+++ client/routes/sponsors/index.tsx
@@ -2,8 +2,6 @@ import { PageView } from '~/routes/PageView';
 import { ImgLazy } from '~/components/ui/ImgLazy';
 import { GoldSponsors } from '../sponsors/Gold';
 import { HowToSupport } from '../sponsors/HowToSupport';
-import { Grid } from '~/components/layout/Grid';
-import { FlexStack } from '~/components/layout/FlexStack';
 import { SectionContainer } from '~/components/ui/Section';
 import { Box } from '~/components/ui/Box';
 import { TitleSection } from '~/components/lwjgl/TitleSection';
@@ -15,69 +13,7 @@ export default function SponsorsRoute() {
       description="LWJGL exists thanks to all the people who contribute and back/sponsor our collective."
     >
       <HowToSupport />
-      <GoldSponsors>
-        <SectionContainer css={{ pt: '$lg' }}>
-          <hr />
-          <TitleSection css={{ pt: '$lg', color: 'silver' }}>Silver Sponsors:</TitleSection>
-          <Grid
-            css={{
-              pt: '$sm',
-              gap: '$safe',
-              alignItems: 'center',
-              '@sm': {
-                grid: 'auto-flow / repeat(2, 1fr)',
-                justifyContent: 'center',
-                textAlign: 'center',
-              },
-              '@md': {
-                grid: 'auto-flow / repeat(3, 1fr)',
-              },
-              '@lg': {
-                grid: 'auto-flow / repeat(4, 1fr)',
-              },
-            }}
-          >
-            <a
-              href="https://www.vpnconexion.es/vpn-gratis/"
-              title="VPN CONEXIÓN"
-              rel="sponsored noopener external"
-              target="_blank"
-            >
-              <FlexStack
-                gap="0.5rem"
-                css={{
-                  color: '#577BFA',
-                  fontWeight: 'bold',
-                  '@md': {
-                    justifyContent: 'center',
-                  },
-                }}
-              >
-                <ImgLazy
-                  width={460 / 8}
-                  height={279 / 8}
-                  src="/img/sponsors/connessionivpn.it-460x279.svg"
-                  alt="VPN CONEXIÓN"
-                />{' '}
-                <span>VPN CONEXIÓN</span>
-              </FlexStack>
-            </a>
-            {/* <a
-              href="https://writersperhour.com/"
-              title="Writers Per Hour"
-              rel="sponsored noopener external"
-              target="_blank"
-            >
-              <ImgLazy
-                width={466 / 3}
-                height={200 / 3}
-                src="/img/sponsors/writers-per-hour.png"
-                alt="Writers Per Hour"
-              />
-            </a> */}
-          </Grid>
-        </SectionContainer>
-      </GoldSponsors>
+      <GoldSponsors />
 
       <SectionContainer padding>
         <TitleSection>Backers:</TitleSection>
