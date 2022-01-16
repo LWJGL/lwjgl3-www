89e4032e4ede7065d0372f78594d8e0b1ddb760f
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2022-01-17 00:40:41 +0200

    chore: add WC global defs

---
+++ types/wc.d.ts
@@ -0,0 +1,9 @@
+// declare namespace JSX {
+//   interface SvgIconProps extends React.HTMLAttributes<HTMLSpanElement> {
+//     name: string;
+//   }
+
+//   interface IntrinsicElements {
+//     'svg-icon': SvgIconProps;
+//   }
+// }
