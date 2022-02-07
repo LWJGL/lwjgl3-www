ce948c7cbf42bbd32fd65e53dfc261b061168398
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2022-02-07 16:57:27 +0200

    fix(scripts): ECS service id

--- scripts/aws/update.sh
+++ scripts/aws/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CLUSTER_ID="arn:aws:ecs:us-east-1:770058214810:cluster/lwjgl-cluster"
-SERVICE_ID="arn:aws:ecs:us-east-1:770058214810:cluster/lwjgl-cluster/lwjgl-www"
+SERVICE_ID="arn:aws:ecs:us-east-1:770058214810:service/lwjgl-cluster/lwjgl-www"
 
 aws --profile lwjgl ecs update-service --cluster "$CLUSTER_ID" --service "$SERVICE_ID" --force-new-deployment
 watch -c "aws --profile lwjgl ecs describe-services --cluster \"$CLUSTER_ID\" --services \"$SERVICE_ID\" | jq -C -r '.services[0] | { lastEvent: .events[0], deployments: .deployments }'"
