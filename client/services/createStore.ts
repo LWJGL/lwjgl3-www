20b1930244376da2b390445741141cbb6dd1d72c
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2022-07-10 21:01:42 +0300

    feat: add lazy subscription support in createStore

--- client/services/createStore.ts
+++ client/services/createStore.ts
@@ -1,14 +1,36 @@
-export function createStore<Store>(initialState: Store) {
+declare const UNDEFINED_VOID_ONLY: unique symbol;
+type Destructor = () => void | { [UNDEFINED_VOID_ONLY]: never };
+type StateSetter<T> = (fn: (state: T) => T) => void;
+type Initiator<T> = (fn: StateSetter<T>) => void | Destructor;
+
+export function createStore<Store>(initialState: Store, lazySubscribe?: Initiator<Store>) {
   let state = initialState;
+  let unsubscribe: void | Destructor;
+
   const getState = () => state;
+
   const listeners = new Set<Function>();
-  const setState = (fn: (state: Store) => Store) => {
+
+  const setState: StateSetter<Store> = (fn) => {
     state = fn(state);
     listeners.forEach((listener) => listener());
   };
+
   const subscribe = (listener: Function) => {
     listeners.add(listener);
-    return () => listeners.delete(listener);
+
+    if (listeners.size === 1 && lazySubscribe != null) {
+      unsubscribe = lazySubscribe(setState);
+    }
+
+    return () => {
+      listeners.delete(listener);
+      if (listeners.size === 0 && unsubscribe != null) {
+        unsubscribe();
+        unsubscribe = undefined;
+      }
+    };
   };
+
   return { getState, setState, subscribe };
 }
