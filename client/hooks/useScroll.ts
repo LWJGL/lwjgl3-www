05163e9df39e88ef93b568fd75a940e8eb62d4cf
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2022-07-10 21:02:08 +0300

    feat: re-write useScroll with useSyncExternalStore

--- client/hooks/useScroll.ts
+++ client/hooks/useScroll.ts
@@ -1,25 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useSyncExternalStore } from 'react';
+import { SUPPORTS_PASSIVE_EVENTS } from '~/services/supports';
+import { createStore } from '~/services/createStore';
 
-type XY = [number, number];
+type ScrollPosition = [number, number];
 
-export function getWindowScroll(): XY {
-  // return [window.scrollX, window.scrollY];
-  return [window.pageXOffset, window.pageYOffset];
-}
+let registerOptions = SUPPORTS_PASSIVE_EVENTS ? { passive: true } : false;
 
-export function useScroll() {
-  const [pos, setPos] = useState<XY>(getWindowScroll());
-
-  function handleScroll() {
-    setPos(getWindowScroll());
+let store = createStore<ScrollPosition>([0, 0], (setState) => {
+  function updateScrollPosition(ev: Event) {
+    setState((prev) => [window.pageXOffset, Math.max(0, window.pageYOffset)]);
   }
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, false);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  window.addEventListener('scroll', updateScrollPosition, registerOptions);
 
-  return pos;
+  return () => {
+    /*
+      We're passing the same registerOptions object even if the spec says it's fine to just pass `false`.
+      Based on recommendation from:
+      https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener#syntax
+      "It's worth noting that some browser releases have been inconsistent on this, and unless you have specific reasons otherwise,
+      it's probably wise to use the same values used for the call to addEventListener() when calling removeEventListener()."
+    */
+    //@ts-expect-error
+    window.removeEventListener('scroll', updateScrollPosition, registerOptions);
+  };
+});
+
+export function useScroll(): ScrollPosition {
+  return useSyncExternalStore<ScrollPosition>(store.subscribe, store.getState, store.getState);
 }
