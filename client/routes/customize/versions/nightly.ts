c97a755cc39237ad533029dc0a17c72abbe63226
Ioannis Tsakpinis <iotsakp@gmail.com>
2022-11-04 22:16:41 +0200

    feat: add FMOD & KTX

--- client/routes/customize/versions/nightly.ts
+++ client/routes/customize/versions/nightly.ts
@@ -1,7 +1,23 @@
-import { BuildBindings, Version } from '../types';
+import { Binding, BuildBindings, NATIVE_ALL, Version } from '../types';
 
 export default (prev: BuildBindings): BuildBindings => ({
   ...prev,
   version: Version.LWJGL332,
   alias: Version.Nightly,
+  byId: {
+    ...prev.byId,
+    [Binding.FMOD]: {
+      id: Binding.FMOD,
+      title: 'FMOD',
+      description: 'An end-to-end solution for adding sound and music to any game.',
+      website: 'https://www.fmod.com',
+    },
+    [Binding.KTX]: {
+      id: Binding.KTX,
+      title: 'KTX (Khronos Texture)',
+      description: 'A lightweight container for textures for OpenGL®, Vulkan® and other GPU APIs.',
+      website: 'https://www.khronos.org/ktx/',
+      natives: NATIVE_ALL,
+    },
+  },
 });
