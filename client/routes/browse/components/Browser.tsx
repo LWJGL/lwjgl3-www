d38da395cb5a1c67584c2baa22e9a29c35395576
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2023-09-16 20:05:49 +0300

    feat: semantic sorting for releases & addons in file browser

--- client/routes/browse/components/Browser.tsx
+++ client/routes/browse/components/Browser.tsx
@@ -6,6 +6,7 @@ import { Folder, SpinnerRow, FolderError } from './Folder';
 import { Box } from '~/components/ui/Box';
 import { Row } from './Row';
 import { getFolderData } from '../loaders/path';
+import { Minimatch } from 'minimatch';
 
 // Browser
 interface Props {
@@ -60,9 +61,41 @@ interface ContentProps {
   targetPath?: string;
 }
 
+var matchAddons = new Minimatch('addons/**');
+
 function FolderContents({ path, targetPath }: ContentProps) {
-  const { folders, files } = use(getFolderData(path));
+  let { folders, files } = use(getFolderData(path));
   const basePath = path.length ? path + '/' : '';
+
+  if (path === 'release') {
+    folders.sort((a, b) => a.localeCompare(b) * -1);
+  } else if (matchAddons.match(path)) {
+    files.sort((a, b) => {
+      let semVerRE = /[-]([0-9]+)[.]([0-9]+)[.]([0-9]+)[-.]/;
+      let aSemVer = a.match(semVerRE);
+      let bSemVer = b.match(semVerRE);
+
+      if (aSemVer === null && bSemVer !== null) {
+        return -1;
+      } else if (aSemVer !== null && bSemVer === null) {
+        return 1;
+      } else if (aSemVer !== null && bSemVer !== null) {
+        let aSemVerNum = parseInt(aSemVer[1]) * 1_000_000 + parseInt(aSemVer[2]) * 1_000 + parseInt(aSemVer[3]);
+        let bSemVerNum = parseInt(bSemVer[1]) * 1_000_000 + parseInt(bSemVer[2]) * 1_000 + parseInt(bSemVer[3]);
+
+        // let compare = aSemVerNum - bSemVerNum;
+        let compare = bSemVerNum - aSemVerNum;
+
+        if (compare !== 0) {
+          return compare;
+        }
+      }
+
+      // Do not account for special characters when sorting, it produces a better result
+      return a.replace(/[-]/g, '').localeCompare(b.replace(/[-]/g, ''));
+    });
+  }
+
   return (
     <>
       {folders.map((folder) => {
