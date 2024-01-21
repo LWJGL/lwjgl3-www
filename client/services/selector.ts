0713713d81b2b57b702266d6e85fde632546d60c
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2024-01-21 16:10:28 +0200

    feat: upgrade to reselect api v5

--- client/services/selector.ts
+++ client/services/selector.ts
@@ -1,6 +1,13 @@
-import { createSelectorCreator, defaultMemoize } from 'reselect';
+import { createSelectorCreator, lruMemoize } from 'reselect';
 import isEqual from 'react-fast-compare';
 import { shallowEqual } from './shallowEqual';
 
-export const createSelectorDeepEqual = createSelectorCreator(defaultMemoize, isEqual);
-export const createSelectorShallowEqual = createSelectorCreator(defaultMemoize, shallowEqual);
+export const createSelectorDeepEqual = createSelectorCreator({
+  memoize: lruMemoize,
+  memoizeOptions: { maxSize: 2, equalityCheck: isEqual },
+});
+
+export const createSelectorShallowEqual = createSelectorCreator({
+  memoize: lruMemoize,
+  memoizeOptions: { maxSize: 2, equalityCheck: shallowEqual },
+});
