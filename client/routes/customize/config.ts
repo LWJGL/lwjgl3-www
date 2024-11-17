b89923ba922b285e264306209b74380e82307bbf
SWinxy <swinxythecat@gmail.com>
2024-11-17 14:18:27 -0500

    Update lwjgl3-awt version in config.ts
    LWJGLX/lwjgl3-awt has had a few versions since 0.1.8. 0.2.2 was released today.

--- client/routes/customize/config.ts
+++ client/routes/customize/config.ts
@@ -261,7 +261,7 @@ function getInitialConfig(): BuildStore {
           maven: {
             groupId: 'org.lwjglx',
             artifactId: 'lwjgl3-awt',
-            version: '0.1.8',
+            version: '0.2.2',
           },
         },
         [Addon.Steamworks4J]: {
