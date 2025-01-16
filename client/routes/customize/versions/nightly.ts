66bb5214a85446e8f69be173561aba2ed7edd12e
Ioannis Tsakpinis <iotsakp@gmail.com>
2025-01-16 10:41:09 +0200

    feat: exclude bindings that were removed in 3.4.0

--- client/routes/customize/versions/nightly.ts
+++ client/routes/customize/versions/nightly.ts
@@ -1,17 +1,31 @@
-import {Binding, BuildBindings, NATIVE_ALL, Version} from '../types';
+import { Binding, BuildBindings, NATIVE_ALL, Version } from '../types';
 
-export default (prev: BuildBindings): BuildBindings => ({
-  ...prev,
-  version: Version.LWJGL340,
-  alias: Version.Nightly,
-  byId: {
-    ...prev.byId,
-    [Binding.SDL]: {
-      id: Binding.SDL,
-      title: 'SDL (Simple DirectMedia Layer)',
-      description: 'A cross-platform development library designed to provide low level access to audio, keyboard, mouse, joystick, and graphics hardware via OpenGL/Direct3D/Metal/Vulkan.',
-      natives: NATIVE_ALL,
-      website: 'https://www.libsdl.org/',
+export default (prev: BuildBindings): BuildBindings => {
+  // These bindings where removed in 3.4.0
+  const {
+    [Binding.CUDA]: _0,
+    [Binding.LIBDIVIDE]: _1,
+    [Binding.MEOW]: _2,
+    [Binding.OPENVR]: _3,
+    [Binding.OVR]: _4,
+    [Binding.SSE]: _5,
+    [Binding.TOOTLE]: _6,
+    ...bindings340
+  } = prev.byId;
+  return {
+    ...prev,
+    version: Version.LWJGL340,
+    alias: Version.Nightly,
+    byId: {
+      ...bindings340,
+      [Binding.SDL]: {
+        id: Binding.SDL,
+        title: 'SDL (Simple DirectMedia Layer)',
+        description:
+          'A cross-platform development library designed to provide low level access to audio, keyboard, mouse, joystick, and graphics hardware via OpenGL/Direct3D/Metal/Vulkan.',
+        natives: NATIVE_ALL,
+        website: 'https://www.libsdl.org/',
+      },
     },
-  },
-});
+  };
+};
